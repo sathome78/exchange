c62a2bfdf31abc8d3b28e62af2332d099999cce6
oleg <olegvanich@gmail.com>
2018-03-30 17:50:22 +0300

    cors added 3

--- Security/src/main/java/me/exrates/security/filter/CORSFilter.java
+++ Security/src/main/java/me/exrates/security/filter/CORSFilter.java
@@ -25,7 +25,7 @@ public class CORSFilter extends GenericFilterBean {
 		HttpServletRequest request = (HttpServletRequest) req;
 
 		// TODO in real life origin (*) must be replaced with our frontend hosts
-		response.setHeader("Access-Control-Allow-Origin", "http://dev2.exrates.tech");
+		response.setHeader("Access-Control-Allow-Origin", "http://test.exrates.tech");
 		response.setHeader("Access-control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, X-Forwarded-For, x-auth-token, Exrates-Rest-Token");
 		response.setHeader("Access-Control-Max-Age", "3600");
