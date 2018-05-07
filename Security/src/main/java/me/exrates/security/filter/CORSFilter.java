5746be24dfb3cdde1f11e1638fcf712d21043a6f
maks115015 <avto12@i.ua>
2018-05-07 15:36:36 +0300

    fix cors

--- Security/src/main/java/me/exrates/security/filter/CORSFilter.java
+++ Security/src/main/java/me/exrates/security/filter/CORSFilter.java
@@ -33,6 +33,7 @@ public class CORSFilter extends GenericFilterBean {
 		}
 		response.setHeader("Access-Control-Allow-Origin", "*");*/
 		response.setHeader("Access-Control-Allow-Origin", "http://localhost:9000");
+		response.setHeader("Access-Control-Allow-Origin", "http://dev2.exrates.tech");
 		response.setHeader("Access-control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, X-Forwarded-For, x-auth-token, Exrates-Rest-Token");
 		response.setHeader("Access-Control-Max-Age", "3600");
