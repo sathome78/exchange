ad9d4bcfd0130d3821db25fea68713a4f74feaf7
dziubak <vlad.dziubak@gmail.com>
2018-07-23 13:15:01 +0300

    EX-605 | Hotfix sql request

--- Dao/src/main/java/me/exrates/dao/impl/InputOutputDaoImpl.java
+++ Dao/src/main/java/me/exrates/dao/impl/InputOutputDaoImpl.java
@@ -406,7 +406,7 @@ public class InputOutputDaoImpl implements InputOutputDao {
 
   @Override
   public List<InputOutputSummaryByUsersDto> getInputOutputSummaryByUsers(LocalDateTime startTime, LocalDateTime endTime, List<Integer> userRoleIdList) {
-    String sql = "SELECT US.id AS user_id, US.email, MIN(CUR.ID) as currency_id, CUR.name AS currency_name, SUM(refill) as input, SUM(withdraw) as output"+
+    String sql = "SELECT US.id AS user_id, US.email, MIN(CUR.ID) as currency_id, CUR.name AS currency_name, SUM(refill) as input, SUM(withdraw) as output "+
             "FROM (SELECT U.id AS user_id, U.email AS email, TX.currency_id, TX.amount AS refill, 0 AS withdraw FROM TRANSACTION TX "+
             "JOIN WALLET W ON TX.user_wallet_id = W.id JOIN USER U ON W.user_id = U.id AND U.roleid IN (:user_roles) " +
             "WHERE TX.operation_type_id = 1 AND TX.source_type = 'REFILL' " +
