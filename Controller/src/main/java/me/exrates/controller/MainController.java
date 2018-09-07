ae32af968b555864d700f7db693c1fba09f3b5b0
dziubak <vlad.dziubak@gmail.com>
2018-09-07 20:36:49 +0300

    Lisk | Fix prop
    (cherry picked from commit ea0334d)

--- Controller/src/main/java/me/exrates/controller/MainController.java
+++ Controller/src/main/java/me/exrates/controller/MainController.java
@@ -116,11 +116,7 @@ public class MainController {
     }
 
     @RequestMapping("/403")
-    public String error403() throws Exception{
-        String secret = String.join(" ", MnemonicCode.INSTANCE.toMnemonic(SecureRandom.getSeed(16)));
-        System.out.println("************************************* SECRET ************************************* | "+secret);
-        return "403";
-    }
+    public String error403() { return "403"; }
 
     /**
      * Register user on referral link (redirect to dashboard, call pop-up with registration)
