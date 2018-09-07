365e7f6551f8135eb4507db7db2da88e032d5f63
dziubak <vlad.dziubak@gmail.com>
2018-09-07 19:13:36 +0300

    On CLX bitcoin coin

--- Controller/src/main/java/me/exrates/config/CryptocurrencyConfig.java
+++ Controller/src/main/java/me/exrates/config/CryptocurrencyConfig.java
@@ -220,12 +220,13 @@ public class CryptocurrencyConfig {
     public BitcoinService cmkService(){
         return new BitcoinServiceImpl("merchants/cmk_wallet.properties", "CMK", "CMK", 4, 20, false, true);
     }
+    */
 
     @Bean(name = "clxServiceImpl")
     public BitcoinService clxService() {
         return new BitcoinServiceImpl("merchants/clx_wallet.properties",
                 "CLX", "CLX", 4, 20, false, false, true, false);
-    }*/
+    }
     
     // LISK-like cryptos
 
