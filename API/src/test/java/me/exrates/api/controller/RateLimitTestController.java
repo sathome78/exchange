21404ed05fc0ccaf8ee370c5ecdef2d13047d442
uraberez <berezin.kiev@gmail.com>
2018-09-17 18:29:50 +0300

    EX-1063 API request limits. RateLimitTestController

---
+++ API/src/test/java/me/exrates/api/controller/RateLimitTestController.java
@@ -0,0 +1,13 @@
+package me.exrates.api.controller;
+
+/**
+ * Created by Yuriy Berezin on 17.09.2018.
+ */
+
+public class RateLimitTestController {
+
+    public void testEndpoint(){
+
+    }
+
+}
