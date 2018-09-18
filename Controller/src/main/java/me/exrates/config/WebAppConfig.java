9163baa50f451064254db4426a31d630365e0b64
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2018-09-18 12:07:41 +0300

    EX-1059 (fix flyway, add new functionality)

--- Controller/src/main/java/me/exrates/config/WebAppConfig.java
+++ Controller/src/main/java/me/exrates/config/WebAppConfig.java
@@ -247,6 +247,7 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
         Flyway flyway = new Flyway();
         flyway.setDataSource(dataSource);
         flyway.setBaselineOnMigrate(true);
+        flyway.repair();
         flyway.migrate();
         return dataSource;
     }
