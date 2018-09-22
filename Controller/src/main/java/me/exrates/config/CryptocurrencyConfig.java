2f2c6c6964ffac857ec5aca23e481d3119f6f12e
dziubak <vlad.dziubak@gmail.com>
2018-09-22 11:43:39 +0300

    On bitcoin coins (several coins)

--- Controller/src/main/java/me/exrates/config/CryptocurrencyConfig.java
+++ Controller/src/main/java/me/exrates/config/CryptocurrencyConfig.java
@@ -222,6 +222,8 @@ public class CryptocurrencyConfig {
     public BitcoinService cmkService(){
         return new BitcoinServiceImpl("merchants/cmk_wallet.properties", "CMK", "CMK", 4, 20, false, true);
     }
+    */
+
 
     @Bean(name = "clxServiceImpl")
     public BitcoinService clxService() {
@@ -239,7 +241,6 @@ public class CryptocurrencyConfig {
         return new BitcoinServiceImpl("merchants/qrk_wallet.properties",
                 "QRK", "QRK", 4, 20, false, false);
     }
-    */
 
     // LISK-like cryptos
 
