6ad6e74413d37eeb1090a6ebaf7bed59c8909409
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2018-09-25 11:12:36 +0300

    fix V1.4 migration

--- Controller/src/main/resources/db/migration/V1.4__API_request_limits.sql
+++ Controller/src/main/resources/db/migration/V1.4__API_request_limits.sql
@@ -1,8 +1,9 @@
-DROP TABLE IF EXISTS user_api;
+DROP TABLE IF EXISTS USER_API;
 
-CREATE TABLE user_api
+CREATE TABLE USER_API
 (
-  user_id INT NOT NULL,
-  attempts  INT NOT NULL,
-  CONSTRAINT user_api_user_id_fk FOREIGN KEY (user_id) REFERENCES user (id)
+  id        INT   UNSIGNED  PRIMARY KEY   NOT NULL AUTO_INCREMENT,
+  user_id   INT                           NOT NULL,
+  attempts  INT                           NOT NULL,
+  CONSTRAINT user_api_user_id_fk FOREIGN KEY (user_id) REFERENCES USER (id)
 );
