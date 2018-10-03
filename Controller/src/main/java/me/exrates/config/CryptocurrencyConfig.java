c5e40bc4f0d839f589f202b8b1e3e299338d6137
dziubak <vlad.dziubak@gmail.com>
2018-10-03 19:03:31 +0300

    Bitcoin fork - DDX
    (cherry picked from commit 3d9fb5e)

--- Controller/src/main/java/me/exrates/config/CryptocurrencyConfig.java
+++ Controller/src/main/java/me/exrates/config/CryptocurrencyConfig.java
@@ -234,6 +234,14 @@ public class CryptocurrencyConfig {
         return new BitcoinServiceImpl("merchants/cmk_wallet.properties", "CMK", "CMK", 4, 20, false, true);
     }
 
+    @Bean(name = "ddxServiceImpl")
+    public BitcoinService ddxService() {
+        return new BitcoinServiceImpl("merchants/ddx_wallet.properties",
+                "DDX", "DDX", 4, 20, false, true);
+    }
+
+
+
     @Bean(name="lpcServiceImpl")
     public BitcoinService lpcService(){
         return new BitcoinServiceImpl("merchants/lpc_wallet.properties", "LPC", "LPC", 4, 20, false, false);
