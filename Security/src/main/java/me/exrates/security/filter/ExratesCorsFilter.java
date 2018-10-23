44abde65dbfb7dc1725e75d2c0b6a726fff2c3eb
oleg <olegvanich@gmail.com>
2018-10-23 19:34:15 +0300

    cors local

--- Security/src/main/java/me/exrates/security/filter/ExratesCorsFilter.java
+++ Security/src/main/java/me/exrates/security/filter/ExratesCorsFilter.java
@@ -35,16 +35,6 @@ public class ExratesCorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
 //        response.setHeader("Access-Control-Allow-Origin", "http://dev4.exrates.tech");
 
-//        System.out.println("protocol " + reqUrl.getProtocol());
-//        System.out.println("domain " + reqUrl.getHost());
-//        System.out.println("req_port " + reqUrl.getPort());
-
-//        String path = String.join("", reqUrl.getProtocol(), "://", reqUrl.getHost());
-//        if (reqUrl.getHost().equals("localhost")) {
-//            path = String.join("", path, ":", String.valueOf(reqUrl.getPort()));
-//        }
-//        System.out.println("header path " + path);
-//        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, X-Forwarded-For, x-auth-token, Exrates-Rest-Token");
         response.setHeader("Access-Control-Max-Age", "3600");
