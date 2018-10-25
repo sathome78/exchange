0949aeb129ee0ba2e0b70705234243a7092095ea
StasGorobzeev <stanislav.gorobzeev@upholding.biz>
2018-10-25 19:33:55 +0300

    refactoring

--- Controller/src/main/java/me/exrates/ngcontroller/service/NgOrderService.java
+++ Controller/src/main/java/me/exrates/ngcontroller/service/NgOrderService.java
@@ -1,9 +1,15 @@
 package me.exrates.ngcontroller.service;
 
 import me.exrates.model.User;
+import me.exrates.model.dto.CandleDto;
 import me.exrates.model.dto.OrderCreateDto;
 import me.exrates.model.dto.WalletsAndCommissionsForOrderCreationDto;
+import me.exrates.model.enums.OperationType;
 import me.exrates.ngcontroller.mobel.InputCreateOrderDto;
+import me.exrates.ngcontroller.mobel.ResponseInfoCurrencyPairDto;
+
+import java.util.List;
+import java.util.Map;
 
 public interface NgOrderService {
     OrderCreateDto prepareOrder(InputCreateOrderDto inputOrder);
@@ -12,5 +18,12 @@ public interface NgOrderService {
 
     boolean processUpdateStopOrder(User user, InputCreateOrderDto inputOrder);
 
-    WalletsAndCommissionsForOrderCreationDto getWalletAndCommision();
+    WalletsAndCommissionsForOrderCreationDto getWalletAndCommision(String email, OperationType operationType,
+                                                                   int activeCurrencyPair);
+
+    ResponseInfoCurrencyPairDto getCurrencyPairInfo(int currencyPairId, User user);
+
+    String createOrder(InputCreateOrderDto inputOrder);
+
+    Map<String, Object> filterDataPeriod(List<CandleDto> data, long fromSeconds, long toSeconds, String resolution);
 }
--- Controller/src/main/java/me/exrates/ngcontroller/service/impl/NgOrderServiceImpl.java
+++ Controller/src/main/java/me/exrates/ngcontroller/service/impl/NgOrderServiceImpl.java
@@ -2,33 +2,56 @@ package me.exrates.ngcontroller.service.impl;
 
 import me.exrates.dao.OrderDao;
 import me.exrates.dao.StopOrderDao;
+import me.exrates.model.Currency;
 import me.exrates.model.CurrencyPair;
 import me.exrates.model.ExOrder;
+import me.exrates.model.StockExchangeStats;
 import me.exrates.model.StopOrder;
 import me.exrates.model.User;
+import me.exrates.model.dto.CandleDto;
 import me.exrates.model.dto.OrderCreateDto;
 import me.exrates.model.dto.OrderValidationDto;
+import me.exrates.model.dto.WalletsAndCommissionsForOrderCreationDto;
+import me.exrates.model.dto.onlineTableDto.ExOrderStatisticsShortByPairsDto;
 import me.exrates.model.enums.CurrencyPairType;
 import me.exrates.model.enums.OperationType;
+import me.exrates.model.enums.OrderActionEnum;
 import me.exrates.model.enums.OrderBaseType;
 import me.exrates.model.enums.OrderStatus;
 import me.exrates.ngcontroller.NgDashboardException;
 import me.exrates.ngcontroller.mobel.InputCreateOrderDto;
+import me.exrates.ngcontroller.mobel.ResponseInfoCurrencyPairDto;
 import me.exrates.ngcontroller.service.NgOrderService;
 import me.exrates.service.CurrencyService;
+import me.exrates.service.DashboardService;
 import me.exrates.service.OrderService;
+import me.exrates.service.StockExchangeService;
 import me.exrates.service.UserService;
 import me.exrates.service.WalletService;
-import me.exrates.service.exception.api.OrderParamsWrongException;
 import me.exrates.service.stopOrder.StopOrderService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class NgOrderServiceImpl implements NgOrderService {
+
+    private static final Logger logger = LogManager.getLogger(NgOrderServiceImpl.class);
 
     private final UserService userService;
     private final CurrencyService currencyService;
@@ -37,6 +60,8 @@ public class NgOrderServiceImpl implements NgOrderService {
     private final WalletService walletService;
     private final StopOrderDao stopOrderDao;
     private final StopOrderService stopOrderService;
+    private final StockExchangeService stockExchangeService;
+    private final DashboardService dashboardService;
 
     @Autowired
     public NgOrderServiceImpl(UserService userService,
@@ -45,7 +70,9 @@ public class NgOrderServiceImpl implements NgOrderService {
                               OrderDao orderDao,
                               WalletService walletService,
                               StopOrderDao stopOrderDao,
-                              StopOrderService stopOrderService) {
+                              StopOrderService stopOrderService,
+                              DashboardService dashboardService,
+                              StockExchangeService stockExchangeService) {
         this.userService = userService;
         this.currencyService = currencyService;
         this.orderService = orderService;
@@ -53,6 +80,8 @@ public class NgOrderServiceImpl implements NgOrderService {
         this.walletService = walletService;
         this.stopOrderDao = stopOrderDao;
         this.stopOrderService = stopOrderService;
+        this.dashboardService = dashboardService;
+        this.stockExchangeService = stockExchangeService;
     }
 
     @Override
@@ -88,7 +117,7 @@ public class NgOrderServiceImpl implements NgOrderService {
             throw new NgDashboardException(errorMap.toString());
         }
 
-        if (prepareNewOrder.getTotalWithComission().compareTo(inputOrder.getTotal()) != 0 ) {
+        if (prepareNewOrder.getTotalWithComission().compareTo(inputOrder.getTotal()) != 0) {
             throw new NgDashboardException(String.format("Total value %.2f doesn't equal to calculate %.2f",
                     inputOrder.getTotal(), prepareNewOrder.getTotalWithComission()));
         }
@@ -205,4 +234,252 @@ public class NgOrderServiceImpl implements NgOrderService {
         }
         return result;
     }
+
+    @Override
+    public WalletsAndCommissionsForOrderCreationDto getWalletAndCommision(String email,
+                                                                          OperationType operationType,
+                                                                          int currencyPairId) {
+
+        CurrencyPair activeCurrencyPair = currencyService.findCurrencyPairById(currencyPairId);
+
+        if (activeCurrencyPair == null) {
+            throw new RuntimeException("Wrong currency pair");
+        }
+
+        Currency spendCurrency = null;
+
+        if (operationType == OperationType.SELL) {
+            spendCurrency = activeCurrencyPair.getCurrency1();
+        } else if (operationType == OperationType.BUY) {
+            spendCurrency = activeCurrencyPair.getCurrency2();
+        }
+
+        WalletsAndCommissionsForOrderCreationDto walletAndCommission =
+                orderService.getWalletAndCommission(email, spendCurrency, operationType);
+
+        return walletAndCommission;
+
+    }
+
+    @Override
+    public ResponseInfoCurrencyPairDto getCurrencyPairInfo(int currencyPairId, User user) {
+
+        ResponseInfoCurrencyPairDto result = new ResponseInfoCurrencyPairDto();
+        CurrencyPair currencyPair = currencyService.findCurrencyPairById(currencyPairId);
+        BigDecimal balanceByCurrency1 =
+                dashboardService.getBalanceByCurrency(user.getId(), currencyPair.getCurrency1().getId());
+
+        List<ExOrderStatisticsShortByPairsDto> currencyRate =
+                orderService.getStatForSomeCurrencies(Collections.singletonList(currencyPairId));
+        result.setBalanceByCurrency1(balanceByCurrency1);
+
+        BigDecimal balanceByCurrency2 = new BigDecimal(0);
+        for (ExOrderStatisticsShortByPairsDto dto : currencyRate) {
+            if (dto == null) continue;
+
+            result.setCurrencyRate(dto.getLastOrderRate());
+            result.setPercentChange(dto.getPercentChange());
+            result.setLastCurrencyRate(dto.getPredLastOrderRate());
+
+            BigDecimal rateNow = new BigDecimal(dto.getLastOrderRate());
+            BigDecimal rateYesterday = new BigDecimal(dto.getPredLastOrderRate());
+            BigDecimal subtract = rateNow.subtract(rateYesterday);
+            result.setChangedValue(subtract.toString());
+            BigDecimal rate = new BigDecimal(dto.getLastOrderRate());
+            balanceByCurrency2 = balanceByCurrency1.multiply(rate);
+
+            break;
+
+        }
+//        if (!currencyRate.isEmpty()) {
+//            result.setCurrencyRate(currencyRate.get(0).getLastOrderRate());
+//            result.setPercentChange(currencyRate.get(0).getPercentChange());
+//            result.setLastCurrencyRate(currencyRate.get(0).getPredLastOrderRate());
+//
+//            BigDecimal rateNow = new BigDecimal(currencyRate.get(0).getLastOrderRate());
+//            BigDecimal rateYesterday = new BigDecimal(currencyRate.get(0).getPredLastOrderRate());
+//            BigDecimal subtract = rateNow.subtract(rateYesterday);
+//            result.setChangedValue(subtract.toString());
+//            BigDecimal rate = new BigDecimal(currencyRate.get(0).getLastOrderRate());
+//            balanceByCurrency2 = balanceByCurrency1.multiply(rate);
+//        }
+        result.setBalanceByCurrency2(balanceByCurrency2);
+
+        //get daily statistic by 2 ways ---  what way is correct ???
+
+        //1 method
+        LocalDateTime fromLocalDate = LocalDateTime.now().minusDays(1).withHour(1);
+        LocalDateTime toLocalDate = LocalDateTime.now().minusDays(1).withHour(23);
+        Date from = Date.from(fromLocalDate.atZone(ZoneId.systemDefault()).toInstant());
+        Date to = Date.from(toLocalDate.atZone(ZoneId.systemDefault()).toInstant());
+
+        List<StockExchangeStats> statistics;
+        try {
+            statistics =
+                    stockExchangeService.getStockExchangeStatisticsByPeriod(currencyPairId, from, to);
+            //set rateHigh
+            statistics.stream()
+                    .map(StockExchangeStats::getPriceHigh)
+                    .max(Comparator.naturalOrder())
+                    .ifPresent(high -> result.setRateHigh(high.toString()));
+
+            //set rateLow
+            statistics.stream()
+                    .map(StockExchangeStats::getPriceLow)
+                    .max(Comparator.reverseOrder())
+                    .ifPresent(low -> result.setRateLow(low.toString()));
+
+            //set volume24h
+            statistics.stream()
+                    .map(StockExchangeStats::getVolume)
+                    .max(Comparator.naturalOrder())
+                    .ifPresent(volume -> result.setVolume24h(volume.toString()));
+        } catch (ArithmeticException e) {
+            logger.error("Error calculating max and min values");
+        }
+
+        return result;
+    }
+
+    @Override
+    public String createOrder(InputCreateOrderDto inputOrder) {
+
+        OrderCreateDto prepareNewOrder = prepareOrder(inputOrder);
+
+        String result;
+        switch (prepareNewOrder.getOrderBaseType()) {
+            case STOP_LIMIT: {
+                result = stopOrderService.create(prepareNewOrder, OrderActionEnum.CREATE, null);
+                break;
+            }
+            default: {
+                result = orderService.createOrder(prepareNewOrder, OrderActionEnum.CREATE, null);
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public Map<String, Object> filterDataPeriod(List<CandleDto> data, long fromSeconds, long toSeconds, String resolution) {
+        List<CandleDto> filteredData = new ArrayList<>(data);
+        HashMap<String, Object> filterDataResponse = new HashMap<>();
+        if (filteredData.isEmpty()) {
+            filterDataResponse.put("s", "ok");
+            getData(filterDataResponse, filteredData, resolution);
+            return filterDataResponse;
+        }
+
+        if ((filteredData.get(data.size() - 1).getTime() / 1000) < fromSeconds) {
+            filterDataResponse.put("s", "no_data");
+            filterDataResponse.put("nextTime", filteredData.get(data.size() - 1).getTime() / 1000);
+            return filterDataResponse;
+
+        }
+
+        int fromIndex = -1;
+        int toIndex = -1;
+
+        for (int i = 0; i < filteredData.size(); i++) {
+            long time = filteredData.get(i).getTime() / 1000;
+            if (fromIndex == -1 && time >= fromSeconds) {
+                fromIndex = i;
+            }
+            if (toIndex == -1 && time >= toSeconds) {
+                toIndex = time > toSeconds ? i - 1 : i;
+            }
+            if (fromIndex != -1 && toIndex != -1) {
+                break;
+            }
+        }
+
+        fromIndex = fromIndex > 0 ? fromIndex : 0;
+        toIndex = toIndex > 0 ? toIndex + 1 : filteredData.size();
+
+
+        toIndex = Math.min(fromIndex + 1000, toIndex); // do not send more than 1000 bars for server capacity reasons
+
+        String s = "ok";
+
+        if (toSeconds < filteredData.get(0).getTime() / 1000) {
+            s = "no_data";
+        }
+        filterDataResponse.put("s", s);
+
+        toIndex = Math.min(fromIndex + 1000, toIndex);
+
+        if (fromIndex > toIndex) {
+            filterDataResponse.put("s", "no_data");
+            filterDataResponse.put("nextTime", filteredData.get(data.size() - 1).getTime() / 1000);
+            return filterDataResponse;
+        }
+
+        filteredData = filteredData.subList(fromIndex, toIndex);
+        getData(filterDataResponse, filteredData, resolution);
+        return filterDataResponse;
+
+    }
+
+    private void getData(HashMap<String, Object> response, List<CandleDto> result, String resolution) {
+        List<Long> t = new ArrayList<>();
+        List<BigDecimal> o = new ArrayList<>();
+        List<BigDecimal> h = new ArrayList<>();
+        List<BigDecimal> l = new ArrayList<>();
+        List<BigDecimal> c = new ArrayList<>();
+        List<BigDecimal> v = new ArrayList<>();
+
+        LocalDateTime first = LocalDateTime.ofEpochSecond((result.get(0).getTime() / 1000), 0, ZoneOffset.UTC)
+                .truncatedTo(ChronoUnit.DAYS);
+        t.add(first.toEpochSecond(ZoneOffset.UTC));
+        o.add(BigDecimal.ZERO);
+        h.add(BigDecimal.ZERO);
+        l.add(BigDecimal.ZERO);
+        c.add(BigDecimal.ZERO);
+        v.add(BigDecimal.ZERO);
+
+        for (CandleDto r : result) {
+            LocalDateTime now = LocalDateTime.ofEpochSecond((r.getTime() / 1000), 0, ZoneOffset.UTC)
+                    .truncatedTo(ChronoUnit.MINUTES);
+            LocalDateTime actualDateTime;
+            long currentMinutesOfHour = now.getLong(ChronoField.MINUTE_OF_HOUR);
+            long currentHourOfDay = now.getLong(ChronoField.HOUR_OF_DAY);
+
+            switch (resolution) {
+                case "30":
+                    long minutes = Math.abs(currentMinutesOfHour - 30);
+                    actualDateTime = now.minusMinutes(currentMinutesOfHour <= 30 ? currentMinutesOfHour : minutes);
+                    break;
+                case "60":
+                    actualDateTime = now.minusMinutes(currentMinutesOfHour);
+                    break;
+                case "240":
+                    actualDateTime = now.minusMinutes(currentMinutesOfHour).minusHours(currentHourOfDay % 4);
+                    break;
+                case "720":
+                    actualDateTime = now.minusMinutes(currentMinutesOfHour).minusHours(currentHourOfDay % 12);
+                    break;
+                case "M":
+                    actualDateTime = now.truncatedTo(ChronoUnit.DAYS).withDayOfMonth(1);
+                    break;
+                default:
+                    actualDateTime = now.minusMinutes(currentMinutesOfHour);
+
+            }
+
+            t.add(actualDateTime.toEpochSecond(ZoneOffset.UTC));
+            o.add(r.getOpen());
+            h.add(r.getHigh());
+            l.add(r.getLow());
+            c.add(r.getClose());
+            v.add(r.getVolume());
+        }
+        response.put("t", t);
+        response.put("o", o);
+        response.put("h", h);
+        response.put("l", l);
+        response.put("c", c);
+        response.put("v", v);
+    }
+
+
 }
