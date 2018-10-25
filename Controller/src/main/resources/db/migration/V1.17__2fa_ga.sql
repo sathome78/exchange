2dcb5cef7b0e6d32e9439bd816f82e145d8720d6
dziubak <vlad.dziubak@gmail.com>
2018-10-25 11:55:32 +0300

    Hotfix #2

--- Controller/src/main/resources/db/migration/V1.17__2fa_ga.sql
+++ Controller/src/main/resources/db/migration/V1.17__2fa_ga.sql
@@ -1,8 +1,8 @@
-INSERT INTO 2FA_NOTIFICATOR VALUES (4, 'google2faNotificatorServiceImpl', 'FREE', true, 'GOOGLE_AUTHENTICATOR');
+INSERT IGNORE INTO 2FA_NOTIFICATOR VALUES (4, 'google2faNotificatorServiceImpl', 'FREE', true, 'GOOGLE_AUTHENTICATOR');
 
-INSERT INTO `2FA_NOTIFICATION_MESSAGES` (event, type, message) VALUES ('LOGIN', 'GOOGLE2FA', 'response.login.code.google2fa');
-INSERT INTO `2FA_NOTIFICATION_MESSAGES` (event, type, message) VALUES ('WITHDRAW', 'GOOGLE2FA', 'response.withdraw.code.google2fa');
-INSERT INTO `2FA_NOTIFICATION_MESSAGES` (event, type, message) VALUES ('TRANSFER', 'GOOGLE2FA', 'response.transfer.code.google2fa');
+INSERT IGNORE INTO `2FA_NOTIFICATION_MESSAGES` (event, type, message) VALUES ('LOGIN', 'GOOGLE2FA', 'response.login.code.google2fa');
+INSERT IGNORE INTO `2FA_NOTIFICATION_MESSAGES` (event, type, message) VALUES ('WITHDRAW', 'GOOGLE2FA', 'response.withdraw.code.google2fa');
+INSERT IGNORE INTO `2FA_NOTIFICATION_MESSAGES` (event, type, message) VALUES ('TRANSFER', 'GOOGLE2FA', 'response.transfer.code.google2fa');
 
 
 CREATE TABLE 2FA_GOOGLE_AUTHENTICATOR (
