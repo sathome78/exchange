eb86e0c78151f97a05bbfa62a441212f0584b120
oleg <olegvanich@gmail.com>
2018-10-28 22:22:57 +0200

    updated sockets protocol

--- Security/src/main/java/me/exrates/security/filter/ExratesCorsFilter.java
+++ Security/src/main/java/me/exrates/security/filter/ExratesCorsFilter.java
@@ -29,7 +29,6 @@ public class ExratesCorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) resp;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        URL reqUrl = new URL(request.getRequestURL().toString());
 //        logger.error("REQUEST HOST: {}", reqUrl.getHost());
 //        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
 //        response.setHeader("Access-Control-Allow-Origin", "http://dev4.exrates.tech");
