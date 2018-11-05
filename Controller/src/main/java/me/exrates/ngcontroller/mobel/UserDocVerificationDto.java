22131025d71dc875ef1078f1947c806a7d306cdb
StasGorobzeev <stanislav.gorobzeev@upholding.biz>
2018-11-05 22:25:43 +0200

    add save user info

--- Controller/src/main/java/me/exrates/ngcontroller/mobel/UserDocVerificationDto.java
+++ Controller/src/main/java/me/exrates/ngcontroller/mobel/UserDocVerificationDto.java
@@ -1,7 +1,5 @@
 package me.exrates.ngcontroller.mobel;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,13 +13,4 @@ public class UserDocVerificationDto {
     private Integer userId;
     private VerificationDocumentType documentType;
     private String encoded;
-
-//    @JsonCreator
-//    public UserDocVerificationDto(@JsonProperty("userId") int userId,
-//                                  @JsonProperty("documentType") String documentType,
-//                                  @JsonProperty("encoded") String encoded) {
-//        this.userId = userId;
-//        this.documentType = VerificationDocumentType.of(documentType);
-//        this.encoded = encoded;
-//    }
 }
