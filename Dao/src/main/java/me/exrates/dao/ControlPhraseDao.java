9ffe45bcaf580b2f0f7eadb4b271fcbe68d90c06
NMaslukov <JavaNoob14679987>
2018-11-08 17:43:32 +0200

    fix

--- Dao/src/main/java/me/exrates/dao/ControlPhraseDao.java
+++ Dao/src/main/java/me/exrates/dao/ControlPhraseDao.java
@@ -5,6 +5,7 @@ import me.exrates.dao.exception.PhraseNotAllowedException;
 
 public interface ControlPhraseDao {
     String getByUserId(long userId);
+
     void updatePhrese(long userId, String phrase) throws PhraseNotAllowedException;
 
     void deletePhrase(long userId);
