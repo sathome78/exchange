4d328fab5b97d7106a44d18d5766c03ea8e57894
Yuriy Berezin <yuriy.berezin@upholding.biz>
2018-11-12 15:17:28 +0200

    EX-1412 Admin commissions merchants

--- Controller/src/main/webapp/client/js/dataTable/adminCommissionsDataTable.js
+++ Controller/src/main/webapp/client/js/dataTable/adminCommissionsDataTable.js
@@ -142,9 +142,14 @@ function updateMerchantCommissionsDataTable() {
                 },
                 {
                     "data": "isMerchantCommissionSubtractedForWithdraw",
-                    "render": function (data) {
+                    "render": function ( data, type) {
+                        if (type === "sort" || type === 'type') {
+                            return data;
+                        }
+                        else {
                         return '<span>'.concat(data ? '<i class="fa fa-check green text-1_5"></i>' : '<i class="fa fa-close red text-1_5"></i>')
                             .concat('</span>');
+                        }
                     },
                     "className": "text-center"
                 }
