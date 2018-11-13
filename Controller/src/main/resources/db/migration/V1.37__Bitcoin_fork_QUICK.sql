f6628c5f2d7d9e0e00911cbb1f71544b6712545a
NMaslukov <mikita.malykov@upholding.biz>
2018-11-14 01:02:44 +0200

    fix

--- Controller/src/main/resources/db/migration/V1.37__Bitcoin_fork_QUICK.sql
+++ Controller/src/main/resources/db/migration/V1.37__Bitcoin_fork_QUICK.sql
@@ -22,7 +22,7 @@ INSERT IGNORE INTO CURRENCY_LIMIT(currency_id, operation_type_id, user_role_id, 
 
 INSERT IGNORE INTO `COMPANY_WALLET` (`currency_id`) VALUES ((select id from CURRENCY where name = 'QUICK'));
 
-INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, quick_name)
+INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, ticker_name)
 VALUES((select id from CURRENCY where name = 'QUICK'), (select id from CURRENCY where name = 'USD'), 'QUICK/USD', 170, 0, 'QUICK/USD');
 
 INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
@@ -30,7 +30,7 @@ INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_ty
   JOIN USER_ROLE UR
   JOIN ORDER_TYPE OT where CP.name='QUICK/USD';
 
-INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,quick_name)
+INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,ticker_name)
 VALUES((select id from CURRENCY where name = 'QUICK'), (select id from CURRENCY where name = 'BTC'), 'QUICK/BTC', 160, 0, 'BTC', 'QUICK/BTC');
 
 INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
@@ -38,7 +38,7 @@ INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_ty
     JOIN USER_ROLE UR
     JOIN ORDER_TYPE OT where CP.name='QUICK/BTC';
 
-INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,quick_name)
+INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,ticker_name)
 VALUES((select id from CURRENCY where name = 'QUICK'), (select id from CURRENCY where name = 'ETH'), 'QUICK/ETH', 160, 0, 'ETH', 'QUICK/ETH');
 
 INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
