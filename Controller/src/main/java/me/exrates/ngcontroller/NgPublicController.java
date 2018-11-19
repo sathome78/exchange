00755a9fe27cc052c7d67abcd825a86c7ba5b54c
StasGorobzeev <stas.gorobzeev@tech-stack.io>
2018-11-19 16:50:46 +0200

    add test method for check currency rates
    (cherry picked from commit 3a08175)

--- Controller/src/main/java/me/exrates/ngcontroller/NgPublicController.java
+++ Controller/src/main/java/me/exrates/ngcontroller/NgPublicController.java
@@ -174,6 +174,17 @@ public class NgPublicController {
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
+    @GetMapping("/info/all")
+    @ResponseBody
+    public String getCurrencyPairInfoAll() {
+        try {
+            return orderService.getAllCurrenciesMarkersForAllPairs();
+        } catch (Exception e){
+            logger.error("Error - {}", e);
+        }
+        return "";
+    }
+
     private String fromChatMessage(ChatMessage message) {
         String send = "";
         ChatHistoryDto dto = new ChatHistoryDto();
