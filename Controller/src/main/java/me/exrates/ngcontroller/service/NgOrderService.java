35f0ea2ab78a83d79ecf3e803216a04d61db7387
oleg <olegvanich@gmail.com>
2018-11-20 16:50:35 +0200

    added open orders functionality fixed 1

--- Controller/src/main/java/me/exrates/ngcontroller/service/NgOrderService.java
+++ Controller/src/main/java/me/exrates/ngcontroller/service/NgOrderService.java
@@ -6,7 +6,7 @@ import me.exrates.model.dto.OrderCreateDto;
 import me.exrates.model.dto.WalletsAndCommissionsForOrderCreationDto;
 import me.exrates.model.enums.OperationType;
 import me.exrates.model.enums.OrderType;
-import me.exrates.ngcontroller.model.InputCreateOrderDto;
+import me.exrates.ngcontroller.mobel.InputCreateOrderDto;
 import me.exrates.ngcontroller.mobel.OrderBookWrapperDto;
 import me.exrates.ngcontroller.mobel.ResponseInfoCurrencyPairDto;
 import me.exrates.ngcontroller.mobel.ResponseUserBalances;
