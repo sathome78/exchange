ffad64c7c492828e947cf95a25378ba3ab4b92c8
NMaslukov <JavaNoob14679987>
2018-11-22 14:08:28 +0200

    transaction history

--- Controller/src/main/java/me/exrates/ngcontroller/NgBalanceController.java
+++ Controller/src/main/java/me/exrates/ngcontroller/NgBalanceController.java
@@ -2,21 +2,31 @@ package me.exrates.ngcontroller;
 
 import me.exrates.model.User;
 import me.exrates.model.dto.StatisticForMarket;
+import me.exrates.model.dto.TableParams;
+import me.exrates.model.dto.onlineTableDto.MyInputOutputHistoryDto;
+import me.exrates.model.enums.PagingDirection;
+import me.exrates.model.vo.CacheData;
 import me.exrates.ngcontroller.model.RefillPendingRequestDto;
 import me.exrates.ngcontroller.model.UserBalancesDto;
 import me.exrates.ngcontroller.service.BalanceService;
 import me.exrates.ngcontroller.service.RefillPendingRequestService;
 import me.exrates.ngcontroller.util.PagedResult;
+import me.exrates.security.annotation.OnlineMethod;
+import me.exrates.service.InputOutputService;
 import me.exrates.service.MerchantService;
 import me.exrates.service.UserService;
 import me.exrates.service.cache.MarketRatesHolder;
+import org.apache.logging.log4j.core.util.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.LocaleResolver;
 
+import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
+import java.security.Principal;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -38,6 +48,12 @@ public class NgBalanceController {
 
     @Autowired
     private RefillPendingRequestService refillPendingRequestService;
+
+    @Autowired
+    InputOutputService inputOutputService;
+
+    @Autowired
+    LocaleResolver localeResolver;
 
     @GetMapping
     public ResponseEntity<PagedResult<UserBalancesDto>> getBalances(@RequestParam(required = false, name = "page", defaultValue = "1") Integer page,
@@ -65,6 +81,48 @@ public class NgBalanceController {
     public List<RefillPendingRequestDto> getPendingRequests(){
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
         return refillPendingRequestService.getPendingRefillRequests(userService.getIdByEmail(email));
+    }
+
+    /**
+     * it's one of onlines methods, which retrieves data from DB for repaint on view in browser page
+     * returns list the data of user's input/output orders to show in pages "History input/output"
+     *
+     * @param refreshIfNeeded: - "true" if view ought to repainted if data in DB was changed only.
+     *                         - "false" if data must repainted in any cases
+     * @param tableId          determines table on pages "History" to show data
+     * @param page,            direction - used for pgination. Details see in class TableParams
+     * @param principal
+     * @param request
+     * @return list the data of user's orders
+     */
+    @OnlineMethod
+    @RequestMapping(value = "/getInputOutputData/{tableId}", method = RequestMethod.GET)
+    public List<MyInputOutputHistoryDto> getMyInputoutputData(
+            @RequestParam(required = false) Boolean refreshIfNeeded,
+            @PathVariable("tableId") String tableId,
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) PagingDirection direction,
+            Principal principal,
+            HttpServletRequest request) {
+        if (principal == null) {
+            return null;
+        }
+        String email = principal.getName();
+        /**/
+        String attributeName = tableId + "Params";
+        TableParams tableParams = (TableParams) request.getSession().getAttribute(attributeName);
+        Assert.requireNonNull(tableParams, "The parameters are not populated for the " + tableId);
+        tableParams.setOffsetAndLimitForSql(page, direction);
+        /**/
+        String cacheKey = "myInputoutputData" + tableId + request.getHeader("windowid");
+        refreshIfNeeded = refreshIfNeeded == null ? false : refreshIfNeeded;
+        CacheData cacheData = new CacheData(request, cacheKey, !refreshIfNeeded);
+        List<MyInputOutputHistoryDto> result = inputOutputService.getMyInputOutputHistory(cacheData, email, tableParams.getOffset(), tableParams.getLimit(), localeResolver.resolveLocale(request));
+        if (!result.isEmpty()) {
+            result.get(0).setPage(tableParams.getPageNumber());
+        }
+        tableParams.updateEofState(result);
+        return result;
     }
 
 }
