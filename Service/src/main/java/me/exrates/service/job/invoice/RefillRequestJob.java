395ca36c5db2a2f363fd0478a64e338bf7d87e26
NMaslukov <mikita.malykov@upholding.biz>
2018-11-26 17:10:51 +0200

    fix dime

--- Service/src/main/java/me/exrates/service/job/invoice/RefillRequestJob.java
+++ Service/src/main/java/me/exrates/service/job/invoice/RefillRequestJob.java
@@ -45,7 +45,7 @@ public class RefillRequestJob {
    */
   @Scheduled(initialDelay = 180000, fixedDelay = 1000 * 60 * 5)
   public void refillCheckPaymentsForCoins() {
-      String[] merchantNames = new String[]{"QRK", "LBTC", "LPC", "XFC", "DDX", "ABBC", "CBC"};
+      String[] merchantNames = new String[]{"QRK", "LBTC", "LPC", "XFC", "DDX", "ABBC", "CBC", "DIME"};
 
       for (String coin : merchantNames) {
           getBitcoinServiceByMerchantName(coin).scanForUnprocessedTransactions(null);
