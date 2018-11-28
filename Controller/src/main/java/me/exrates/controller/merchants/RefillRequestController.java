94e165da83729d1f44873194e7a883885c007938
NMaslukov <mikita.malykov@upholding.biz>
2018-11-28 16:31:35 +0200

    first controller

--- Controller/src/main/java/me/exrates/controller/merchants/RefillRequestController.java
+++ Controller/src/main/java/me/exrates/controller/merchants/RefillRequestController.java
@@ -1,23 +1,26 @@
 package me.exrates.controller.merchants;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
 import me.exrates.controller.annotation.AdminLoggable;
 import me.exrates.controller.exception.ErrorInfo;
-import me.exrates.model.CreditsOperation;
 import me.exrates.model.InvoiceBank;
-import me.exrates.model.Payment;
-import me.exrates.model.dto.*;
+import me.exrates.model.dto.RefillRequestAcceptDto;
+import me.exrates.model.dto.RefillRequestAcceptEntryParamsDto;
+import me.exrates.model.dto.RefillRequestFlatDto;
+import me.exrates.model.dto.RefillRequestParamsDto;
 import me.exrates.model.dto.onlineTableDto.MyInputOutputHistoryDto;
-import me.exrates.model.enums.invoice.RefillStatusEnum;
 import me.exrates.model.exceptions.InvoiceActionIsProhibitedForCurrencyPermissionOperationException;
 import me.exrates.model.exceptions.InvoiceActionIsProhibitedForNotHolderException;
-import me.exrates.model.userOperation.enums.UserOperationAuthority;
 import me.exrates.model.vo.InvoiceConfirmData;
 import me.exrates.model.vo.PaginationWrapper;
-import me.exrates.service.*;
-import me.exrates.service.exception.IllegalOperationTypeException;
-import me.exrates.service.exception.InvalidAmountException;
+import me.exrates.service.InputOutputService;
+import me.exrates.service.MerchantService;
+import me.exrates.service.RefillService;
+import me.exrates.service.UserService;
 import me.exrates.service.exception.NotEnoughUserWalletMoneyException;
-import me.exrates.service.exception.UserOperationAccessException;
 import me.exrates.service.exception.invoice.InvalidAccountException;
 import me.exrates.service.exception.invoice.InvoiceNotFoundException;
 import me.exrates.service.exception.invoice.MerchantException;
@@ -34,13 +37,14 @@ import org.springframework.web.servlet.LocaleResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.io.UnsupportedEncodingException;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.security.Principal;
-import java.util.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
-import static me.exrates.model.enums.OperationType.INPUT;
-import static me.exrates.model.enums.invoice.InvoiceActionTypeEnum.CREATE_BY_USER;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -76,43 +80,19 @@ public class RefillRequestController {
   @ResponseBody
   public Map<String, Object> createRefillRequest(
           @RequestBody RefillRequestParamsDto requestParamsDto,
-          Principal principal,
-          Locale locale, HttpServletRequest servletRequest, HttpSession session) throws UnsupportedEncodingException {
-    if (requestParamsDto.getOperationType() != INPUT) {
-      throw new IllegalOperationTypeException(requestParamsDto.getOperationType().name());
-    }
-    boolean accessToOperationForUser = userOperationService.getStatusAuthorityForUserByOperation(userService.getIdByEmail(servletRequest.getUserPrincipal().getName()), UserOperationAuthority.INPUT);
-    if(!accessToOperationForUser) {
-      throw new UserOperationAccessException(messageSource.getMessage("merchant.operationNotAvailable", null, localeResolver.resolveLocale(servletRequest)));
-    }
-    if (!refillService.checkInputRequestsLimit(requestParamsDto.getCurrency(), principal.getName())) {
-      throw new RequestLimitExceededException(messageSource.getMessage("merchants.InputRequestsLimit", null, locale));
-    }
-    Boolean forceGenerateNewAddress = requestParamsDto.getGenerateNewAddress() != null && requestParamsDto.getGenerateNewAddress();
-    if (!forceGenerateNewAddress) {
-      Optional<String> address = refillService.getAddressByMerchantIdAndCurrencyIdAndUserId(
-          requestParamsDto.getMerchant(),
-          requestParamsDto.getCurrency(),
-          userService.getIdByEmail(principal.getName())
-      );
-      if (address.isPresent()) {
-        String message = messageSource.getMessage("refill.messageAboutCurrentAddress", new String[]{address.get()}, locale);
-        return new HashMap<String, Object>() {{
-          put("address", address.get());
-          put("message", message);
-          put("qr", address.get());
-        }};
-      }
-    }
-    RefillStatusEnum beginStatus = (RefillStatusEnum) RefillStatusEnum.X_STATE.nextState(CREATE_BY_USER);
-    Payment payment = new Payment(INPUT);
-    payment.setCurrency(requestParamsDto.getCurrency());
-    payment.setMerchant(requestParamsDto.getMerchant());
-    payment.setSum(requestParamsDto.getSum() == null ? 0 : requestParamsDto.getSum().doubleValue());
-    CreditsOperation creditsOperation = inputOutputService.prepareCreditsOperation(payment, principal.getName(), locale)
-        .orElseThrow(InvalidAmountException::new);
-    RefillRequestCreateDto request = new RefillRequestCreateDto(requestParamsDto, creditsOperation, beginStatus, locale);
-    return refillService.createRefillRequest(request);
+          HttpSession session) throws IOException {
+
+      OkHttpClient cl = new OkHttpClient();
+
+      Request req = new Request.Builder()
+              .url("http://" + "localhost:8081" + "/refill/request/create")
+              .post(com.squareup.okhttp.RequestBody.create(com.squareup.okhttp.MediaType.parse(MediaType.APPLICATION_JSON), new ObjectMapper().writeValueAsString(requestParamsDto)))
+              .addHeader("access_token", (String) session.getAttribute("access_token"))
+              .addHeader("refresh_token", (String) session.getAttribute("refresh_token"))
+              .addHeader("Content-Type", MediaType.APPLICATION_FORM_URLENCODED)
+              .build();
+
+    return new ObjectMapper().readValue(cl.newCall(req).execute().body().string(), new TypeReference<Map<String, Object>>(){});
   }
 
 
