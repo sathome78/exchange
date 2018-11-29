106b6fbcf315f411bd4b3e55a8146e9dae2a47dc
StasGorobzeev <stas.gorobzeev@tech-stack.io>
2018-11-29 18:47:15 +0200

    add priceInUsd to MarketRatesHolder
    (cherry picked from commit 62339a4)

--- Service/src/main/java/me/exrates/service/cache/MarketRatesHolder.java
+++ Service/src/main/java/me/exrates/service/cache/MarketRatesHolder.java
@@ -29,6 +29,9 @@ import java.util.stream.Collectors;
 @Service
 public class MarketRatesHolder {
 
+    private static Integer ETH_USD_ID = 0;
+    private static Integer BTC_USD_ID = 0;
+
     private final OrderDao orderDao;
     private Map<Integer, StatisticForMarket> ratesMarketMap = new ConcurrentHashMap<>();
 
@@ -46,16 +49,21 @@ public class MarketRatesHolder {
         markets.forEach(o -> {
             processPercentChange(o);
             ratesMarketMap.put(o.getCurrencyPairId(), o);
+            if (o.getCurrencyPairName().equalsIgnoreCase("BTC/USD")) {
+                BTC_USD_ID = o.getCurrencyPairId();
+            } else if (o.getCurrencyPairName().equalsIgnoreCase("ETH/USD")) {
+                ETH_USD_ID = o.getCurrencyPairId();
+            }
         });
         log.info("Finish fill ratesMarketMap, time = {}", new Date());
     }
 
     public List<StatisticForMarket> getAllFromDb() {
-        return orderDao.getOrderStatisticForNewMarkets().stream().peek(this::processPercentChange).collect(Collectors.toList());
+        return orderDao.getOrderStatisticForNewMarkets().stream().peek(this::calculatePriceInUSD).collect(Collectors.toList());
     }
 
     public List<StatisticForMarket> getAll() {
-        return ratesMarketMap.values().stream().peek(this::processPercentChange).collect(Collectors.toList());
+        return ratesMarketMap.values().stream().peek(this::calculatePriceInUSD).collect(Collectors.toList());
     }
 
     public Map<Integer, StatisticForMarket> getRatesMarketMap() {
@@ -65,7 +73,7 @@ public class MarketRatesHolder {
     private void processPercentChange(StatisticForMarket o) {
         BigDecimal lastExrate = o.getLastOrderRate();
         BigDecimal predLast = o.getPredLastOrderRate() != null ? o.getPredLastOrderRate() : BigDecimal.ZERO;
-        BigDecimal percentChange = new BigDecimal(0);
+        BigDecimal percentChange = BigDecimal.ZERO;
         if (BigDecimalProcessing.moreThanZero(lastExrate) && BigDecimalProcessing.moreThanZero(predLast)) {
             percentChange = BigDecimalProcessing.doAction(predLast, lastExrate, ActionType.PERCENT_GROWTH);
         }
@@ -91,6 +99,7 @@ public class MarketRatesHolder {
             BigDecimal volume = BigDecimalProcessing.doAction(statisticForMarket.getVolume(), amount, ActionType.ADD);
             statisticForMarket.setVolume(volume);
             this.processPercentChange(statisticForMarket);
+            this.calculatePriceInUSD(statisticForMarket);
         }
     }
 
@@ -101,9 +110,32 @@ public class MarketRatesHolder {
         List<StatisticForMarket> result = new ArrayList<>();
         ids.forEach(p -> {
             StatisticForMarket statistic = ratesMarketMap.get(p);
-            this.processPercentChange(statistic);
+            this.calculatePriceInUSD(statistic);
             result.add(statistic);
         });
         return result;
+    }
+
+    private void calculatePriceInUSD(StatisticForMarket pair) {
+        if (pair.getMarket().equalsIgnoreCase("USD")) {
+            pair.setPriceInUsd(pair.getLastOrderRate());
+            return;
+        }
+        BigDecimal dealPrice = pair.getLastOrderRate();
+        if (pair.getMarket().equalsIgnoreCase("BTC")) {
+            pair.setPriceInUsd(getPriceInUsd(dealPrice, BTC_USD_ID));
+        } else if (pair.getMarket().equalsIgnoreCase("ETH")) {
+            pair.setPriceInUsd(getPriceInUsd(dealPrice, ETH_USD_ID));
+        }
+    }
+
+    private BigDecimal getPriceInUsd(BigDecimal dealPrice, int pairId) {
+        if (pairId == 0) {
+            return null;
+        } else if (ratesMarketMap.containsKey(pairId)) {
+            BigDecimal lastRateForBtcUsd = ratesMarketMap.get(pairId).getLastOrderRate();
+            return lastRateForBtcUsd.multiply(dealPrice);
+        }
+        return null;
     }
 }
