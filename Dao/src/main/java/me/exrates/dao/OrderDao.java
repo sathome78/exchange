1f624ea6ee1214b5dbaba17b4ae9c25e86d1a85e
StasGorobzeev <stas.gorobzeev@tech-stack.io>
2018-12-04 10:11:19 +0200

    add generate excel file

--- Dao/src/main/java/me/exrates/dao/OrderDao.java
+++ Dao/src/main/java/me/exrates/dao/OrderDao.java
@@ -113,6 +113,9 @@ public interface OrderDao {
                                                 String scope, Integer offset, Integer limit, Map<String, String> sortedColumns,
                                                 LocalDate dateFrom, LocalDate dateTo, boolean hideCanceled);
 
+    List<OrderWideListDto> getAllOrders(Integer userId, OrderStatus status, CurrencyPair currencyPair, Locale locale,
+                                                String scope, LocalDate dateFrom, LocalDate dateTo, boolean hideCanceled);
+
     Integer getMyOrdersWithStateCount(int userId, CurrencyPair currencyPair, OrderStatus status, String scope, Integer offset,
                                       Integer limit, Locale locale, LocalDate dateFrom, LocalDate dateTo, boolean hideCanceled);
 
