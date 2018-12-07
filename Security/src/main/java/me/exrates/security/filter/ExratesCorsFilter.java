9f660987f848df440ddce06c8dbf018792ee8bc5
oleg <olegvanich@gmail.com>
2018-12-07 12:40:38 +0200

    added accepted orders fast and comments
    (cherry picked from commit df26c22)

--- Security/src/main/java/me/exrates/security/filter/ExratesCorsFilter.java
+++ Security/src/main/java/me/exrates/security/filter/ExratesCorsFilter.java
@@ -34,7 +34,7 @@ public class ExratesCorsFilter implements Filter {
 //        response.setHeader("Access-Control-Allow-Origin", "http://promo.exrates.top");
 //        response.setHeader("Access-Control-Allow-Origin", "https://dev4.exapp");
 
-        response.setHeader("Access-Control-Allow-Origin", "http://dev6.exapp");
+        response.setHeader("Access-Control-Allow-Origin", "https://demo.exrates.me");
 
 
         response.setHeader("Access-control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS, DELETE");
