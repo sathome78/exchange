cef554a9c74119a5d7173801813e31e81b26ea72
StasGorobzeev <stas.gorobzeev@tech-stack.io>
2018-12-18 16:20:46 +0200

    fix DEVEX-660

--- Controller/src/main/java/me/exrates/ngcontroller/service/impl/NgOrderServiceImpl.java
+++ Controller/src/main/java/me/exrates/ngcontroller/service/impl/NgOrderServiceImpl.java
@@ -143,7 +143,13 @@ public class NgOrderServiceImpl implements NgOrderService {
             throw new NgDashboardException(errorMap.toString());
         }
 
-        if (prepareNewOrder.getTotalWithComission().compareTo(inputOrder.getTotal()) != 0) {
+        BigDecimal totalWithComission = prepareNewOrder.getTotalWithComission();
+        BigDecimal inputTotal = inputOrder.getTotal();
+
+        if (totalWithComission.setScale(5, RoundingMode.HALF_UP)
+            .compareTo(inputTotal.setScale(5, RoundingMode.HALF_UP)) != 0) {
+
+//        if (prepareNewOrder.getTotalWithComission().compareTo(inputOrder.getTotal()) != 0) {
             logger.error("Comparing total, from user - {}, from server - {}", inputOrder.getTotal(),
                     prepareNewOrder.getTotalWithComission());
             throw new NgDashboardException(String.format("Total value %.2f doesn't equal to calculate %.2f",
