9582bed714b65a2f2a46a5760bfe0dee3cb05040
oleg <olegvanich@gmail.com>
2018-12-28 17:27:13 +0200

    cors

--- Security/src/main/java/me/exrates/security/filter/ExratesCorsFilter.java
+++ Security/src/main/java/me/exrates/security/filter/ExratesCorsFilter.java
@@ -19,11 +19,11 @@ public class ExratesCorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) resp;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+//        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
 
 //        response.setHeader("Access-Control-Allow-Origin", "http://dev6.exapp");
 
-//        response.setHeader("Access-Control-Allow-Origin", "https://demo.exrates.me");
+        response.setHeader("Access-Control-Allow-Origin", "https://demo.exrates.me");
 
 
         response.setHeader("Access-control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS, DELETE");
