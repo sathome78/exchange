1647b8013631becd3c00d6184f97b2694bfe2f3a
vlad.dziubak <vlad.dziubak@upholding.biz>
2019-01-08 11:51:32 +0200

    C2-235 | Bitcoin fork - Exosis (EXO)

--- Controller/src/main/java/me/exrates/config/BtcGenerator.java
+++ Controller/src/main/java/me/exrates/config/BtcGenerator.java
@@ -212,6 +212,6 @@ public class BtcGenerator {
 
 
     public static void main(String[] args) throws IOException {
-        generate("RIME", "RimeCoin", 20, true, true, "172.10.11.174", 8090, 5580);
+        generate("EXO", "Exosis", 20, true, true, "172.10.13.176", 8332, 11901);
     }
 }
