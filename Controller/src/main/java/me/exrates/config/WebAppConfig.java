b47bf255f8a2ff13831a495ddeb2b7fb9d9cca5c
vlad.dziubak <vlad.dziubak@upholding.biz>
2019-01-08 16:16:45 +0200

    C2-250 | Ethereum token - ZIL

--- Controller/src/main/java/me/exrates/config/WebAppConfig.java
+++ Controller/src/main/java/me/exrates/config/WebAppConfig.java
@@ -1626,6 +1626,16 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
                 "MCO", true, ExConvert.Unit.AIWEI);
     }
 
+    @Bean(name = "zilServiceImpl")
+    public EthTokenService zilService() {
+        List<String> tokensList = new ArrayList<>();
+        tokensList.add("0x05f4a42e251f2d52b8ed15e9fedaacfcef1fad27");
+        return new EthTokenServiceImpl(
+                tokensList,
+                "ZIL",
+                "ZIL", true, ExConvert.Unit.SZABO);
+    }
+
     //    Qtum tokens:
     @Bean(name = "spcServiceImpl")
     public QtumTokenService spcService() {
