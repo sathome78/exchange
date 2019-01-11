368a6a1f1a7fe39f0b23d5fe431388b6fa1f304c
NMaslukov <JavaNoob14679987>
2019-01-11 14:21:25 +0200

    changes

--- Controller/src/main/java/me/exrates/controller/interceptor/TokenInterceptor.java
+++ Controller/src/main/java/me/exrates/controller/interceptor/TokenInterceptor.java
@@ -6,19 +6,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class NodeInterceptor extends HandlerInterceptorAdapter {
-    private final String NODE_TOKEN_VALUE;
-    private final String NODE_TOKEN = "NODE_TOKEN";
+public class TokenInterceptor extends HandlerInterceptorAdapter {
+    private final String AUTH_TOKEN_VALUE;
+    private final String AUTH_TOKEN = "AUTH_TOKEN";
 
-    public NodeInterceptor(String secret) {
-        this.NODE_TOKEN_VALUE = secret;
+    public TokenInterceptor(String secret) {
+        this.AUTH_TOKEN_VALUE = secret;
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
-        String token = request.getHeader(NODE_TOKEN);
+        String token = request.getHeader(AUTH_TOKEN);
 
-        if(token == null || !token.equals(NODE_TOKEN_VALUE)) {
+        if(token == null || !token.equals(AUTH_TOKEN_VALUE)) {
             response.setStatus(403);
             response.getWriter().write("Incorrect token");
             return false;
