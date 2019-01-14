b80277a9a72d7f97b7fe503042def1d5e9b91fb2
NMaslukov <JavaNoob14679987>
2019-01-14 13:05:08 +0200

    KOD try to fix

--- Service/src/main/java/me/exrates/service/job/invoice/RefillRequestJob.java
+++ Service/src/main/java/me/exrates/service/job/invoice/RefillRequestJob.java
@@ -45,9 +45,14 @@ public class RefillRequestJob {
    */
   @Scheduled(initialDelay = 180000, fixedDelay = 1000 * 60 * 5)
   public void refillCheckPaymentsForCoins() {
-      String[] merchantNames = new String[]{"QRK", "LBTC", "LPC", "XFC", "DDX", "MBC", "BTCP", "CLX", "ABBC", "CBC", "BTCZ", "KOD"};
-      for (String coin : merchantNames) {
-          getBitcoinServiceByMerchantName(coin).scanForUnprocessedTransactions(null);
+      try {
+          log.info("Starting refillCheckPaymentsForCoins");
+          String[] merchantNames = new String[]{"QRK", "LBTC", "LPC", "XFC", "DDX", "MBC", "BTCP", "CLX", "ABBC", "CBC", "BTCZ", "KOD"};
+          for (String coin : merchantNames) {
+              getBitcoinServiceByMerchantName(coin).scanForUnprocessedTransactions(null);
+          }
+      } catch (Exception e){
+          log.error(e);
       }
   }
 
