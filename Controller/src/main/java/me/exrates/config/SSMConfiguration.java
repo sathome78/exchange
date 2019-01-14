eebc2b7692efaf9bfb03a6f20db3215268d5d2ba
NMaslukov <JavaNoob14679987>
2019-01-14 16:35:26 +0200

    ssm uncomment

--- Controller/src/main/java/me/exrates/config/SSMConfiguration.java
+++ Controller/src/main/java/me/exrates/config/SSMConfiguration.java
@@ -1,26 +1,26 @@
-//package me.exrates.config;
-//
-//import com.amazonaws.SdkClientException;
-//import me.exrates.SSMGetter;
-//import me.exrates.SSMGetterImpl;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SSMConfiguration {
-//    @Bean
-//    public SSMGetter ssmGetter() {
-//        try {
-//            return new SSMGetterImpl();
-//        }catch (SdkClientException e){
-//            return new MockSSM();
-//        }
-//    }
-//
-//    private class MockSSM implements SSMGetter {
-//        @Override
-//        public String lookup(String s) {
-//            return "MOCK_TOKEN";
-//        }
-//    }
-//}
+package me.exrates.config;
+
+import com.amazonaws.SdkClientException;
+import me.exrates.SSMGetter;
+import me.exrates.SSMGetterImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SSMConfiguration {
+    @Bean
+    public SSMGetter ssmGetter() {
+        try {
+            return new SSMGetterImpl();
+        }catch (SdkClientException e){
+            return new MockSSM();
+        }
+    }
+
+    private class MockSSM implements SSMGetter {
+        @Override
+        public String lookup(String s) {
+            return "MOCK_TOKEN";
+        }
+    }
+}
