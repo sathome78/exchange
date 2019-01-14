b02a3b7ed5914490f8d44e8743b590511efb64fd
StasGorobzeev <stas.gorobzeev@tech-stack.io>
2019-01-14 10:54:15 +0200

    temp logging authorization

--- Controller/src/main/java/me/exrates/ngcontroller/NgUserController.java
+++ Controller/src/main/java/me/exrates/ngcontroller/NgUserController.java
@@ -149,6 +149,8 @@ public class NgUserController {
         String password = RestApiUtils.decodePassword(authenticationDto.getPassword());
         UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationDto.getEmail());
         if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            logger.error("Incorrect password, email = {}, password = {} ", authenticationDto.getEmail(),
+                    password);
             throw new IncorrectPasswordException("Incorrect password");
         }
 
