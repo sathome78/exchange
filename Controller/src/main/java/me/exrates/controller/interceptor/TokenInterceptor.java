8ec700a28185c456c89cf96d029cc3ab89f00755
NMaslukov <JavaNoob14679987>
2019-01-15 17:36:34 +0200

    try to fix fucking shit

--- Controller/src/main/java/me/exrates/controller/interceptor/TokenInterceptor.java
+++ Controller/src/main/java/me/exrates/controller/interceptor/TokenInterceptor.java
@@ -17,7 +17,7 @@ public class TokenInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         String token = request.getHeader(AUTH_TOKEN);
-
+        System.out.println("TOKEN SHOULD BE " + AUTH_TOKEN_VALUE);
         if(token == null || !token.equals(AUTH_TOKEN_VALUE)) {
             response.setStatus(403);
             response.getWriter().write("Incorrect token");
