dcea647baa54db15f38497017020421bf0619f99
vlad.dziubak <vlad.dziubak@upholding.biz>
2019-01-17 15:47:26 +0200

    C2-274 | Ethereum token - QKC

--- Controller/src/main/java/me/exrates/config/WebAppConfig.java
+++ Controller/src/main/java/me/exrates/config/WebAppConfig.java
@@ -943,7 +943,6 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
                 "TNR", true, ExConvert.Unit.ETHER);
     }
 
-    //    Qtum tokens:
     @Bean(name = "inkServiceImpl")
     public EthTokenService InkService() {
         List<String> tokensList = new ArrayList<>();
@@ -1636,7 +1635,14 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
                 "MANA", true, ExConvert.Unit.ETHER);
     }
 
-    //    Qtum tokens:
+    @Bean(name = "qkcServiceImpl")
+	public EthTokenService qkcServiceImpl(){
+		List<String> tokensList = new ArrayList<>();
+		tokensList.add("0xea26c4ac16d4a5a106820bc8aee85fd0b7b2b664");
+		return new EthTokenServiceImpl(tokensList, "QKC","QKC", true, ExConvert.Unit.ETHER);
+	}
+
+	//    Qtum tokens:
     @Bean(name = "spcServiceImpl")
     public QtumTokenService spcService() {
         List<String> tokensList = new ArrayList<>();
