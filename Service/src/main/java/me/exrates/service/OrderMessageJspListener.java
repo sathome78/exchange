4bded2a7e2f457ad81a1659f7edbfa2ab64036a8
DmitrenkoNikitaa <42832927+DmitrenkoNikitaa@users.noreply.github.com>
2019-01-23 09:59:38 +0200

    Add listeners (#530)
    Add implementation of listeners orders from demo excretes.

---
+++ Service/src/main/java/me/exrates/service/OrderMessageJspListener.java
@@ -0,0 +1,45 @@
+package me.exrates.service;
+
+import lombok.extern.log4j.Log4j2;
+import me.exrates.model.dto.InputCreateOrderDto;
+import me.exrates.model.dto.OrderCreateSummaryDto;
+import me.exrates.model.enums.OperationType;
+import me.exrates.model.enums.OrderBaseType;
+import me.exrates.service.handler.OrdersEventHandleService;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static me.exrates.model.enums.OrderBaseType.*;
+
+@Log4j2
+@EnableRabbit
+@Component
+public class OrderMessageJspListener {
+
+
+    @Autowired
+    private OrdersEventHandleService ordersEventHandleService;
+
+    @Autowired
+    private OrderServiceDemoListener orderServiceDemoListener;
+
+
+    /*
+     * This method is triggered if rabbit exchange receives message ({order}) from counterpart application
+     */
+    @RabbitListener(queues = RabbitMqService.JSP_QUEUE)
+    public void processOrder(InputCreateOrderDto order) {
+        log.debug("Order Received: " + order);
+        ordersEventHandleService.handleOrderEventOnMessage(order);
+        OrderCreateSummaryDto orderCreateSummaryDto = orderServiceDemoListener.newOrderToSell(OperationType.valueOf(order.getOrderType()), order.getUserId(), order.getAmount(), order.getRate(), convert(order.getBaseType()), order.getCurrencyPair(), order.getStop());
+        orderServiceDemoListener.recordOrderToDB(order,orderCreateSummaryDto.getOrderCreateDto());
+    }
+
+    // uncomment for testing as this order will be sent from this application
+//    @RabbitListener(queues = RabbitMqService.ANGULAR_QUEUE)
+//    public void processOrderSelf(InputCreateOrderDto order) {
+//        logger.debug("Order Received: " + order);
+//    }
+} 
