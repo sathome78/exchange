44848c415a18296a64bfa0ece3ce0ce27c1be355
NMaslukov <JavaNoob14679987>
2019-01-25 12:15:06 +0200

    changes

--- Service/src/main/java/me/exrates/ActuatorTestDelete.java
+++ Service/src/main/java/me/exrates/ActuatorTestDelete.java
@@ -4,25 +4,27 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Service;
 
-//@Service
-//public class ActuatorTestDelete implements
-//        ApplicationListener<ContextRefreshedEvent>{
-//
-//    private int counter = 0;
-//    @Override
-//    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-//        counter++;
-//        if(counter == 1) new Thread(() -> {test(contextRefreshedEvent); }).start();
-//    }
-//
-//    private void test(ContextRefreshedEvent contextRefreshedEvent) {
-//        CoinTester kodTester = contextRefreshedEvent.getApplicationContext().getBean(CoinTester.class);
-//        try {
-//            System.out.println("INIT BOT");
-//            kodTester.initBot("RIME", new StringBuilder());
-//            kodTester.testCoin(0.00001);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+@Service
+public class ActuatorTestDelete implements
+        ApplicationListener<ContextRefreshedEvent>{
+
+    private int counter = 0;
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        counter++;
+        if(counter == 1) new Thread(() -> {test(contextRefreshedEvent); }).start();
+    }
+
+    private void test(ContextRefreshedEvent contextRefreshedEvent) {
+        CoinTester kodTester = contextRefreshedEvent.getApplicationContext().getBean(CoinTester.class);
+        try {
+            System.out.println("INIT BOT");
+            StringBuilder stringBuilder = new StringBuilder();
+            kodTester.initBot("RIME", stringBuilder);
+            String s = kodTester.testCoin(0.00001);
+            System.out.println(s);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
