aef2bdca8cdf6317f18efa5032dda3e68f4eb6c2
NMaslukov <JavaNoob14679987>
2019-01-29 14:29:44 +0200

    uncomment

--- Controller/src/main/java/me/exrates/config/SSMConfiguration.java
+++ Controller/src/main/java/me/exrates/config/SSMConfiguration.java
@@ -1,6 +1,6 @@
 package me.exrates.config;
 
-/*
+
 import com.amazonaws.SdkClientException;
 import lombok.extern.log4j.Log4j2;
 import me.exrates.SSMGetter;
@@ -29,4 +29,4 @@ public class SSMConfiguration {
         }
     }
 }
-*/
+
