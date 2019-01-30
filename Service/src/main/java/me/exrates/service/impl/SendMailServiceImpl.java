c30f6efb9108df28b42c69c362737cbe4703fbab
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-01-30 03:14:24 +0200

    DEVEX-806 (add endpoint to listing request form)
    fix session interval
    
    (cherry picked from commit da6ed4f)
    
    DEVEX-806 (min refactor / add listing email to properties)

--- Service/src/main/java/me/exrates/service/impl/SendMailServiceImpl.java
+++ Service/src/main/java/me/exrates/service/impl/SendMailServiceImpl.java
@@ -3,7 +3,10 @@ package me.exrates.service.impl;
 import lombok.extern.log4j.Log4j2;
 import me.exrates.model.Email;
 import me.exrates.model.enums.EmailSenderType;
+import me.exrates.model.mail.ListingRequest;
 import me.exrates.service.SendMailService;
+import me.exrates.service.util.MessageFormatterUtil;
+import org.apache.commons.lang3.time.StopWatch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,16 +19,15 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PreDestroy;
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 @Log4j2(topic = "email_log")
 @Service
 @PropertySource(value = {"classpath:/mail.properties"})
-public class SendMailServiceImpl implements SendMailService{
+public class SendMailServiceImpl implements SendMailService {
 
     @Autowired
     @Qualifier("SupportMailSender")
@@ -48,46 +50,70 @@ public class SendMailServiceImpl implements SendMailService{
     @Value("${default_mail_type}")
     private String mailType;
 
-    private final static int THREADS_NUMBER = 8;
-    private final static ExecutorService executors = Executors.newFixedThreadPool(THREADS_NUMBER);
-    private final static ExecutorService supportMailExecutors = Executors.newFixedThreadPool(3);
+    @Value("${support.email}")
+    private String supportEmail;
 
-    private final String SUPPORT_EMAIL = "mail@exrates.top";
-    private final String MANDRILL_EMAIL = "no-reply@exrates.me";
-    private final String INFO_EMAIL = "no-reply@exrates.top";
+    @Value("${mandrill.email}")
+    private String mandrillEmail;
+
+    @Value("${info.email}")
+    private String infoEmail;
+
+    @Value("${listing.email}")
+    private String listingEmail;
+
+    @Value("${listing.subject}")
+    private String listingSubject;
+
+    private final static ExecutorService EXECUTORS = Executors.newFixedThreadPool(8);
+    private final static ExecutorService SUPPORT_MAIL_EXECUTORS = Executors.newFixedThreadPool(3);
+
+    private static final String UTF8 = "UTF-8";
 
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
-    public void sendMail(Email email){
-        supportMailExecutors.execute(() -> {
+    public void sendMail(Email email) {
+        SUPPORT_MAIL_EXECUTORS.execute(() -> {
             try {
-                sendMail(email, SUPPORT_EMAIL, supportMailSender);
-            } catch (Exception e) {
-                log.error(e);
-                sendMail(email, INFO_EMAIL, infoMailSender);
+                sendMail(email.toBuilder()
+                                .from(supportEmail)
+                                .build(),
+                        supportMailSender);
+            } catch (Exception ex) {
+                log.error(ex);
+                sendMail(email.toBuilder()
+                                .from(infoEmail)
+                                .build(),
+                        infoMailSender);
             }
         });
     }
 
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
-    public void sendMailMandrill(Email email){
-        supportMailExecutors.execute(() -> {
+    public void sendMailMandrill(Email email) {
+        SUPPORT_MAIL_EXECUTORS.execute(() -> {
             try {
                 sendByType(email, EmailSenderType.valueOf(mailType));
             } catch (Exception e) {
                 log.error(e);
-                sendMail(email, SUPPORT_EMAIL, supportMailSender);
+                sendMail(email.toBuilder()
+                                .from(supportEmail)
+                                .build(),
+                        supportMailSender);
             }
         });
     }
 
     private void sendByType(Email email, EmailSenderType type) {
         switch (type) {
-            case gmail : {
+            case gmail: {
                 sendInfoMail(email);
                 break;
             }
             case mandrill: {
-                sendMail(email, MANDRILL_EMAIL, mandrillMailSender);
+                sendMail(email.toBuilder()
+                                .from(mandrillEmail)
+                                .build(),
+                        mandrillMailSender);
                 break;
             }
         }
@@ -102,25 +128,29 @@ public class SendMailServiceImpl implements SendMailService{
                 return;
             }
         }
-        executors.execute(() -> {
+        EXECUTORS.execute(() -> {
             try {
-                sendMail(email, INFO_EMAIL, infoMailSender);
-            } catch (MailException e) {
-                log.error(e);
-                sendMail(email, SUPPORT_EMAIL, supportMailSender);
+                sendMail(email.toBuilder()
+                                .from(infoEmail)
+                                .build(),
+                        infoMailSender);
+            } catch (MailException ex) {
+                log.error(ex);
+                sendMail(email.toBuilder()
+                                .from(supportEmail)
+                                .build(),
+                        supportMailSender);
             }
         });
-
     }
 
-    private void sendMail(Email email, String fromAddress, JavaMailSender mailSender) {
-        LocalDateTime start = LocalDateTime.now();
+    private void sendMail(Email email, JavaMailSender mailSender) {
+        StopWatch stopWatch = StopWatch.createStarted();
         log.debug("try to send email {} to {}, time {}", email.getSubject(), email.getTo());
-        email.setFrom(fromAddress);
         try {
             mailSender.send(mimeMessage -> {
                 MimeMessageHelper message;
-                message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                message = new MimeMessageHelper(mimeMessage, true, UTF8);
                 message.setFrom(email.getFrom());
                 message.setTo(email.getTo());
                 message.setSubject(email.getSubject());
@@ -130,31 +160,38 @@ public class SendMailServiceImpl implements SendMailService{
                         message.addAttachment(attachment.getName(), attachment.getResource(), attachment.getContentType());
                 }
             });
-            log.info("Email sent: {}, duration {} seconds", email, Duration.between(start, LocalDateTime.now()).getSeconds());
-        } catch (Exception e) {
-            log.error("Could not send email {}. Reason: {}", email, e.getMessage());
+            log.info("Email sent: {}, duration {} seconds", email, stopWatch.getTime(TimeUnit.SECONDS));
+        } catch (Exception ex) {
+            log.error("Could not send email {}. Reason: {}", email, ex.getMessage());
         }
-
     }
 
     @Override
     public void sendFeedbackMail(String senderName, String senderMail, String messageBody, String mailTo) {
-        Email email = new Email();
-        email.setFrom(senderMail);
-        email.setTo(mailTo);
-        email.setMessage(messageBody);
-        email.setSubject("Feedback from " + senderName + " -- " + senderMail);
-        sendMail(email);
+        sendMail(Email.builder()
+                .from(senderMail)
+                .to(mailTo)
+                .message(messageBody)
+                .subject(String.format("Feedback from %s -- %s", senderName, senderMail))
+                .build());
     }
 
+    public void sendListingRequestEmail(ListingRequest request) {
+        final String name = request.getName();
+        final String email = request.getEmail();
+        final String telegram = request.getTelegram();
+        final String text = request.getText();
 
-
+        sendInfoMail(Email.builder()
+                .to(listingEmail)
+                .subject(listingSubject)
+                .message(MessageFormatterUtil.format(name, email, telegram, text))
+                .build());
+    }
 
     @PreDestroy
     public void destroy() {
-        executors.shutdown();
-        supportMailExecutors.shutdown();
+        EXECUTORS.shutdown();
+        SUPPORT_MAIL_EXECUTORS.shutdown();
     }
-
-
 }
