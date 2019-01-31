cddd19f781d5cf366f3afae82e2da09bfedf65f8
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-01-31 13:51:15 +0200

    DEVEX-916 (temporary unavailable check signature in callback method / add some logs)

--- Service/src/main/java/me/exrates/service/util/ShuftiProUtils.java
+++ Service/src/main/java/me/exrates/service/util/ShuftiProUtils.java
@@ -1,20 +1,24 @@
 package me.exrates.service.util;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Objects;
 
-@Slf4j
+@Log4j2
 public class ShuftiProUtils {
 
     public static boolean checkMerchantSignature(String signature, String responseBody, String secretKey) {
         String buildMerchantSignature = merchantSignature(secretKey, responseBody);
+        log.info("Merchant signature: {}, generated signature: {}", signature, buildMerchantSignature);
+
         return Objects.equals(signature, buildMerchantSignature);
     }
 
     private static String merchantSignature(String secretKey, String responseBody) {
+        log.info("Secret key: {}", secretKey);
+
         return DigestUtils.sha256Hex(String.join(StringUtils.EMPTY, responseBody, secretKey));
     }
 }
