e50a7270aabd6b76363c5381d837d5f5b865974f
NMaslukov <JavaNoob14679987>
2019-02-09 16:21:28 +0200

    changes

--- Controller/src/main/java/me/exrates/config/CryptocurrencyConfig.java
+++ Controller/src/main/java/me/exrates/config/CryptocurrencyConfig.java
@@ -433,16 +433,16 @@ public class CryptocurrencyConfig {
         return new TronTrc10Token("BTT", "BTT", 0, "1000491", "31303030343931", "1000491");
     }*/
 
-    //Bitshares
-    @Bean(name = "ppyServiceImpl")
-    public BitsharesService bitsharesService(){
-        return new PPYServiceImpl("PPY", "PPY", "merchants/ppy.properties", 0);
-    }
-
-    @Bean(name = "aunitServiceImpl")
-    public BitsharesService aunitServiceImpl(){
-        return new BitsharesServiceImpl("AUNIT", "AUNIT", "merchants/aunit.properties", 100);
-    }
+//    //Bitshares
+//    @Bean(name = "ppyServiceImpl")
+//    public BitsharesService bitsharesService(){
+//        return new PPYServiceImpl("PPY", "PPY", "merchants/ppy.properties", 0);
+//    }
+//
+//    @Bean(name = "aunitServiceImpl")
+//    public BitsharesService aunitServiceImpl(){
+//        return new BitsharesServiceImpl("AUNIT", "AUNIT", "merchants/aunit.properties", 100);
+//    }
 
     //Auto-testing
     @Bean
