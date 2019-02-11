2e734f1c6228794ee779c5e3f8148417580a6d1e
Taranyk <grigoriytaranik@gmail.com>
2019-02-11 17:13:28 +0200

    refill() method

---
+++ Service/src/main/java/me/exrates/service/impl/AisiCurrencyServiceImpl.java
@@ -0,0 +1,107 @@
+package me.exrates.service.impl;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.extern.log4j.Log4j2;
+import me.exrates.service.AisiCurrencyService;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.math.BigDecimal;
+
+@Log4j2
+public class AisiCurrencyServiceImpl implements AisiCurrencyService {
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public AisiCurrencyServiceImpl() {
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                .build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        restTemplate = new RestTemplate(requestFactory);
+
+    }
+
+    public String generateNewAddress() {
+        final MultiValueMap<String, String> requestParameters = new LinkedMultiValueMap<>();
+        requestParameters.add("api_key", "970E22216DA4C486CC22EEF9A58CD30E5B3A8A0D22A62F5D5B57222D16337814CEF3E7B1D7227C4754C733FE39F433F5C4E4E0F8B6D9D8F76F893BBA4");
+        UriComponents builder = UriComponentsBuilder
+                .fromHttpUrl("https://api.aisi.io/account/address/new")
+                .queryParams(requestParameters)
+                .build();
+        ResponseEntity<Address> responseEntity = null;
+        try {
+            responseEntity = restTemplate.getForEntity(builder.toUriString(), Address.class);
+            if (responseEntity.getStatusCodeValue() != 200) {
+                log.warn("Error : {}", responseEntity.getStatusCodeValue());
+            }
+        } catch (Exception ex) {
+            log.warn("Error : {}", ex.getMessage());
+        }
+        Address address = responseEntity.getBody();
+        return getBalanceByAddress(address.address);
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+    private static class Address {
+
+        @JsonProperty("Address")
+        String address;
+
+    }
+
+    public String getBalanceByAddress(String adress){
+        final MultiValueMap<String, String> requestParameters = new LinkedMultiValueMap<>();
+        requestParameters.add("api_key", "970E22216DA4C486CC22EEF9A58CD30E5B3A8A0D22A62F5D5B57222D16337814CEF3E7B1D7227C4754C733FE39F433F5C4E4E0F8B6D9D8F76F893BBA4");
+        UriComponents builder = UriComponentsBuilder
+                .fromHttpUrl("https://api.aisi.io/account/" + adress + "/balance")
+                .queryParams(requestParameters)
+                .build();
+        ResponseEntity<Balance> responseEntity = null;
+        try {
+            responseEntity = restTemplate.getForEntity(builder.toUriString(), Balance.class);
+            if (responseEntity.getStatusCodeValue() != 200) {
+                log.warn("Error : {}", responseEntity.getStatusCodeValue());
+            }
+        } catch (Exception ex) {
+            log.warn("Error : {}", ex.getMessage());
+        }
+        return responseEntity.getBody().balance;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+    private static class Balance {
+
+        @JsonProperty("Balance")
+        String balance;
+    }
+
+    public int getTotalBalance() {
+        return 0;
+    }
+
+    private void test(){
+
+    }
+}
---
+++ Service/src/main/java/me/exrates/service/impl/AisiServiceImpl.java
@@ -0,0 +1,54 @@
+package me.exrates.service.impl;
+
+import me.exrates.model.dto.RefillRequestCreateDto;
+import me.exrates.model.dto.WithdrawMerchantOperationDto;
+import me.exrates.service.AisiCurrencyService;
+import me.exrates.service.AisiService;
+import me.exrates.service.exception.RefillRequestAppropriateNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AisiServiceImpl implements AisiService {
+
+    @Autowired
+    private final AisiCurrencyService aisiCurrencyService;
+
+    @Autowired
+    private MessageSource messageSource;
+
+    public AisiServiceImpl(AisiCurrencyService aisiCurrencyService) {
+        this.aisiCurrencyService = aisiCurrencyService;
+    }
+
+
+    @Override
+    public Map<String, String> refill(RefillRequestCreateDto request) {
+        String address = aisiCurrencyService.generateNewAddress();
+        // change merchants.refill.btc
+        String message = messageSource.getMessage("merchants.refill.btc",
+                new Object[] {address}, request.getLocale());
+        return new HashMap<String, String>(){{
+            put("address", address);
+            put("message", message);
+            put("qr", address);
+        }};
+    }
+
+    @Override
+    public void processPayment(Map<String, String> params) throws RefillRequestAppropriateNotFoundException {
+
+    }
+
+    @Override
+    public Map<String, String> withdraw(WithdrawMerchantOperationDto withdrawMerchantOperationDto) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean isValidDestinationAddress(String address) {
+        return false;
+    }
+}
