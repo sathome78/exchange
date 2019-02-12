8dcd109e26584651d93097305357d6b775b9a3fc
vlad.dziubak <vlad.dziubak@upholding.biz>
2019-02-12 13:07:51 +0200

    C2-347

--- Service/src/main/java/me/exrates/service/ethereum/ethTokensWrappers/TokenWrappersGenerator.java
+++ Service/src/main/java/me/exrates/service/ethereum/ethTokensWrappers/TokenWrappersGenerator.java
@@ -10,8 +10,8 @@ public class TokenWrappersGenerator {
     public static void main(String[] args) throws Exception {
         SolidityFunctionWrapperGenerator.run(new String[]{
                 "generate",
-                "/Users/vlad.dziubak/crypto/eth/VRBS.bin",
-                "/Users/vlad.dziubak/crypto/eth/VRBS.abi",
+                "/Users/vlad.dziubak/crypto/eth/ZUBE.bin",
+                "/Users/vlad.dziubak/crypto/eth/ZUBE.abi",
                 "-o",
                 "/Users/vlad.dziubak/workspace/exrates/Service/src/main/java",
                 "-p",
