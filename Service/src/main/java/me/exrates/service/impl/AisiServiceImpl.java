f8d496056c59425929700c058108828b344116c7
Taranyk <grigoriytaranik@gmail.com>
2019-02-13 10:04:56 +0200

    Aisi refill() update

--- Service/src/main/java/me/exrates/service/impl/AisiServiceImpl.java
+++ Service/src/main/java/me/exrates/service/impl/AisiServiceImpl.java
@@ -1,17 +1,31 @@
 package me.exrates.service.impl;
 
+import me.exrates.model.Currency;
+import me.exrates.model.Merchant;
 import me.exrates.model.dto.RefillRequestCreateDto;
 import me.exrates.model.dto.WithdrawMerchantOperationDto;
-import me.exrates.service.AisiCurrencyService;
-import me.exrates.service.AisiService;
+import me.exrates.service.*;
 import me.exrates.service.exception.RefillRequestAppropriateNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class AisiServiceImpl implements AisiService {
+
+    private final static String MERCHANT_NAME = "AISI";
+    private final static String CURRENCY_NAME = "AISI";
+
+    @Autowired
+    private MerchantService merchantService;
+    @Autowired
+    private CurrencyService currencyService;
+    @Autowired
+    private RefillService refillService;
 
     @Autowired
     private final AisiCurrencyService aisiCurrencyService;
@@ -19,10 +33,18 @@ public class AisiServiceImpl implements AisiService {
     @Autowired
     private MessageSource messageSource;
 
+    private Merchant merchant;
+    private Currency currency;
+
+    @PostConstruct
+    public void init() {
+        currency = currencyService.findByName(CURRENCY_NAME);
+        merchant = merchantService.findByName(MERCHANT_NAME);
+    }
+
     public AisiServiceImpl(AisiCurrencyService aisiCurrencyService) {
         this.aisiCurrencyService = aisiCurrencyService;
     }
-
 
     @Override
     public Map<String, String> refill(RefillRequestCreateDto request) {
@@ -31,7 +53,7 @@ public class AisiServiceImpl implements AisiService {
                 new Object[] {address}, request.getLocale());
         return new HashMap<String, String>(){{
             put("address", address);
-            put("message", message);
+            put("message", message + " " + address);
             put("qr", address);
         }};
     }
