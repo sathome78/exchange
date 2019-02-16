c5cb06bd54f37a23a68237814bba519f84be15a0
maks11110 <avto12@i.ua>
2019-02-16 14:39:23 +0200

    endpoint added, refresh work

--- Service/src/main/java/me/exrates/service/handler/UserPersonalOrdersHandler.java
+++ Service/src/main/java/me/exrates/service/handler/UserPersonalOrdersHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Synchronized;
 import lombok.extern.log4j.Log4j2;
 import me.exrates.model.OrderWsDetailDto;
+import me.exrates.model.SynchronizersObject;
 import me.exrates.service.stomp.StompMessenger;
 
 import java.util.ArrayList;
@@ -12,13 +13,15 @@ import java.util.Map;
 import java.util.Timer;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 
 @Log4j2
 public class UserPersonalOrdersHandler {
 
     private Map<Integer, List<OrderWsDetailDto>> orders = new ConcurrentHashMap<>();
-    private Map<Integer, Semaphore> synchronizersMap = new ConcurrentHashMap<>();
+    private Map<Integer, SynchronizersObject> synchronizersMap = new ConcurrentHashMap<>();
+
 
     private final StompMessenger stompMessenger;
     private final ObjectMapper objectMapper;
@@ -34,22 +37,51 @@ public class UserPersonalOrdersHandler {
     }
 
     void addOrderToQueue(OrderWsDetailDto dto, Integer userId) {
-        synchronizersMap.putIfAbsent(userId, new Semaphore(1));
-        if ((synchronizersMap.get(userId).tryAcquire())) {
+        checkAndCreateSynchronizers(userId);
+
+        synchronized (synchronizersMap.get(userId).getObjectSync()) {
             List<OrderWsDetailDto> value = orders.get(userId);
             if (value == null) {
                 value = new CopyOnWriteArrayList<>();
+                value.add(dto);
+                orders.put(userId, value);
             }
-            value.add(dto);
-            orders.put(userId, value);
-            /*timer task here*/
-            send(userId);
         }
+        orders.get(userId).add(dto);
+        send(userId);
     }
 
+    @Synchronized
+    private void checkAndCreateSynchronizers(Integer userId) {
+        synchronizersMap.putIfAbsent(userId, SynchronizersObject.init());
+    }
+
+
+
+
     private void send(Integer userId) {
-        synchronized (synchronizersMap.get(userId)) {
-            sendMessage(orders.get(userId), userId);
+        //достаю синхронизатор
+        //запускаю первый поток и закрываю путь потокам
+        //остальные потоки отсекаю
+        if (synchronizersMap.get(userId).getSemaphore().tryAcquire()) {
+            try {
+                //sleep потока
+                TimeUnit.MILLISECONDS.sleep(refreshTime);
+                //блокирую дальнейшую запись в лист
+                synchronizersMap.get(userId).getLocker().lock();
+                //делаю send
+                List<OrderWsDetailDto> data = orders.get(userId);
+                sendMessage(data, userId);
+                //очищаю основной лист
+                data.clear();
+            } catch (InterruptedException e) {
+                log.error(e);
+            } finally {
+                //открываю доступ потокам
+                synchronizersMap.get(userId).getSemaphore().release();
+                //открываю запись в лист
+                synchronizersMap.get(userId).getLocker().unlock();
+            }
         }
     }
 
