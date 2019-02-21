0b186ccc119cbc02fdfa6ea4c4a507efd5e3ee2f
NMaslukov <mikita.malykov@upholding.biz>
2019-02-21 21:58:19 +0200

    usd order commented

--- Service/src/main/java/me/exrates/cointest/BtcCoinTester.java
+++ Service/src/main/java/me/exrates/cointest/BtcCoinTester.java
@@ -96,7 +96,7 @@ public class BtcCoinTester implements CoinTester {
             testAutoWithdraw(refillAmount);
 //            testManualWithdraw(refillAmount);
             testOrder(BigDecimal.valueOf(0.0001), BigDecimal.valueOf(0.0001), name + "/BTC", BigDecimal.valueOf(0.00));
-            testOrder(BigDecimal.valueOf(0.0001), BigDecimal.valueOf(0.0001), name + "/USD", BigDecimal.valueOf(0.00));
+//            testOrder(BigDecimal.valueOf(0.0001), BigDecimal.valueOf(0.0001), name + "/USD", BigDecimal.valueOf(0.00));
             testOrder(BigDecimal.valueOf(0.0001), BigDecimal.valueOf(0.0001), name + "/ETH", BigDecimal.valueOf(0.00));
             stringBuilder.append("Everything works fine!<br>");
             return "Works fine";
