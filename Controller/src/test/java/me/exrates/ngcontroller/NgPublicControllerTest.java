e9dc2e00964b09e42eace4bfd34f50b005682085
oleg <olegvanich@gmail.com>
2019-02-24 10:05:07 +0200

    added jway

--- Controller/src/test/java/me/exrates/ngcontroller/NgPublicControllerTest.java
+++ Controller/src/test/java/me/exrates/ngcontroller/NgPublicControllerTest.java
@@ -1,8 +1,6 @@
 package me.exrates.ngcontroller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import me.exrates.controller.openAPI.TestUtils;
 import me.exrates.model.User;
 import me.exrates.service.UserService;
 import me.exrates.service.exception.UserNotFoundException;
@@ -11,12 +9,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -24,14 +22,14 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static junit.framework.TestCase.assertTrue;
 import static junit.framework.TestCase.fail;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+
+//https://www.baeldung.com/integration-testing-in-spring
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AngularAppTestConfig.class})
 @WebAppConfiguration
@@ -57,18 +55,20 @@ public class NgPublicControllerTest {
     public void checkIfNewUserEmailExists_whenUserNotFound() throws Exception {
         when(userService.findByEmail(anyString())).thenThrow(UserNotFoundException.class);
         try {
-            mockMvc.perform(MockMvcRequestBuilders.get("/api/public/v2/if_email_exists?email=test@test.com")
+            mockMvc.perform(MockMvcRequestBuilders.get("/api/public/v2/if_email_exists")
+                    .param("email", "test@test.com")
                     .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
             fail("UserNotFoundException is expected");
         } catch (Exception e) {
-            assertTrue(true);
+            assertTrue(e.getCause() instanceof UserNotFoundException);
         }
     }
 
     @Test
     public void checkIfNewUserEmailExists_whenOk() throws Exception {
         when(userService.findByEmail(anyString())).thenReturn(Mockito.mock(User.class));
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/public/v2/if_email_exists?email=test@test.com")
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/public/v2/if_email_exists")
+                .param("email", "test@test.com")
                 .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
                 .content(objectMapper.writeValueAsString(Boolean.class)))
                 .andExpect(MockMvcResultMatchers.status().isOk());
