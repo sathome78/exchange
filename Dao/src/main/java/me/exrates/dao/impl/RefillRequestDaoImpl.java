372aac096d416166a239dca519501a03b8438ce3
NMaslukov <JavaNoob14679987>
2019-03-06 23:30:19 +0200

    condition

--- Dao/src/main/java/me/exrates/dao/impl/RefillRequestDaoImpl.java
+++ Dao/src/main/java/me/exrates/dao/impl/RefillRequestDaoImpl.java
@@ -12,6 +12,8 @@ import me.exrates.model.dto.RefillRequestCreateDto;
 import me.exrates.model.dto.RefillRequestFlatAdditionalDataDto;
 import me.exrates.model.dto.RefillRequestFlatDto;
 import me.exrates.model.dto.RefillRequestFlatForReportDto;
+import me.exrates.model.condition.MonolitConditional;
+import me.exrates.model.dto.*;
 import me.exrates.model.dto.dataTable.DataTableParams;
 import me.exrates.model.dto.filterData.RefillAddressFilterData;
 import me.exrates.model.dto.filterData.RefillFilterData;
@@ -26,6 +28,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -39,14 +43,7 @@ import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import static java.util.Collections.singletonMap;
 import static java.util.Objects.isNull;
@@ -61,6 +58,7 @@ import static me.exrates.model.enums.TransactionSourceType.REFILL;
  */
 
 @Repository
+@Conditional(MonolitConditional.class)
 public class RefillRequestDaoImpl implements RefillRequestDao {
 
     private static final Logger log = LogManager.getLogger("refill");
