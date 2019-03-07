5c638413e7bb029175060fd6c2b363c01530fb3c
vlad.dziubak <vlad.dziubak@upholding.biz>
2019-03-07 12:42:04 +0200

    Pagination

--- Service/src/main/java/me/exrates/service/impl/BitcoinServiceImpl.java
+++ Service/src/main/java/me/exrates/service/impl/BitcoinServiceImpl.java
@@ -7,7 +7,9 @@ import lombok.extern.log4j.Log4j2;
 import me.exrates.dao.MerchantSpecParamsDao;
 import me.exrates.model.Currency;
 import me.exrates.model.Merchant;
+import me.exrates.model.PagingData;
 import me.exrates.model.dto.*;
+import me.exrates.model.dto.dataTable.DataTable;
 import me.exrates.model.dto.merchants.btc.*;
 import me.exrates.model.util.BigDecimalProcessing;
 import me.exrates.service.BitcoinService;
@@ -27,6 +29,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Nullable;
 import javax.annotation.PostConstruct;
@@ -44,7 +47,6 @@ import java.util.stream.Collectors;
 @Data
 public class BitcoinServiceImpl implements BitcoinService {
 
-  private static final int TRANSACTIONS_PER_PAGE_FOR_SEARCH = 500;
   @Value("${btcInvoice.blockNotifyUsers}")
   private Boolean BLOCK_NOTIFYING;
 
@@ -441,17 +443,19 @@ public class BitcoinServiceImpl implements BitcoinService {
   }
 
   @Override
-  public List<BtcTransactionHistoryDto> findTransactions(String value) throws BitcoindException, CommunicationException {
+  public DataTable<List<BtcTransactionHistoryDto>> listTransactions(Map<String, String> tableParams) throws BitcoindException, CommunicationException{
+      Integer start = Integer.parseInt(tableParams.getOrDefault("start", "0"));
+      Integer length = Integer.parseInt(tableParams.getOrDefault("length", "10"));
+      String searchValue = tableParams.get("search[value]");
 
-    List<BtcTransactionHistoryDto> result = new ArrayList<>();
-    List<BtcTransactionHistoryDto> transactions;
+      PagingData<List<BtcTransactionHistoryDto>> searchResult = bitcoinWalletService.listTransaction(start, length, searchValue);
 
-    for (int i = 0; (transactions = bitcoinWalletService.getTransactionsByPage(i, TRANSACTIONS_PER_PAGE_FOR_SEARCH)).size() > 0; i++){
-      List<BtcTransactionHistoryDto> matches = transactions.stream().filter(e -> e.getAddress().equals(value) || e.getBlockhash().equals(value) || e.getTxId().equals(value)).collect(Collectors.toList());
-      result.addAll(matches);
-    }
+      DataTable<List<BtcTransactionHistoryDto>> output = new DataTable<>();
+      output.setData(searchResult.getData());
+      output.setRecordsTotal(searchResult.getTotal());
+      output.setRecordsFiltered(searchResult.getFiltered());
 
-    return result;
+    return output;
   }
 
   @Override
