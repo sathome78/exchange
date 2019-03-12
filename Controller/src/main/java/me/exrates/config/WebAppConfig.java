97464cdad6c1b344e0a7658a3b433b41e2b118a3
NMaslukov <JavaNoob14679987>
2019-03-12 14:59:20 +0200

    properties, ssm

--- Controller/src/main/java/me/exrates/config/WebAppConfig.java
+++ Controller/src/main/java/me/exrates/config/WebAppConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.log4j.Log4j2;
+import me.exrates.SSMGetter;
 import me.exrates.aspect.LoggingAspect;
 import me.exrates.controller.handler.ChatWebSocketHandler;
 import me.exrates.controller.interceptor.MDCInterceptor;
@@ -223,8 +224,8 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 
     private final String inoutTokenValue;
 
-    public WebAppConfig(SsmProperties ssmProperties) {
-        this.inoutTokenValue = ssmProperties.getInoutToken();
+    public WebAppConfig(SSMGetter ssmGetter, SsmProperties ssmProperties) {
+        this.inoutTokenValue = ssmGetter.lookup(ssmProperties.getInoutTokenPath());
     }
 
     @PostConstruct
