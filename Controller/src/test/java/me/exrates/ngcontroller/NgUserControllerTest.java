e45b2eb292a80ac6aef0c0ad17dc30b83f8535d4
Andrew Zarazka <andreyzarazka@gmail.com>
2019-03-16 12:24:20 +0200

    Created NgUserControllerTest. ExchangeRatesHolderImpl - uncomment annotation @PostConstruct before push to staging (method private void init())

---
+++ Controller/src/test/java/me/exrates/ngcontroller/NgUserControllerTest.java
@@ -0,0 +1,44 @@
+package me.exrates.ngcontroller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NgUserControllerTest {
+
+    @Test
+    public void authenticate() {
+    }
+
+    @Test
+    public void register() {
+    }
+
+    @Test
+    public void savePassword() {
+    }
+
+    @Test
+    public void requestForRecoveryPassword() {
+    }
+
+    @Test
+    public void createRecoveryPassword() {
+    }
+
+    @Test
+    public void checkTempToken() {
+    }
+
+    @Test
+    public void otherErrorsHandler() {
+    }
+
+    @Test
+    public void unauthorizedErrorsHandler() {
+    }
+
+    @Test
+    public void incorrectPinExceptionHandler() {
+    }
+}
