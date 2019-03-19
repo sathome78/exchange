9de6af3d52c74e0e9ca23d2ae68559039736fb1b
Andrew Zarazka <andreyzarazka@gmail.com>
2019-03-19 10:15:24 +0200

    Created a test class and checked that the project is built and run locally.

---
+++ Controller/src/test/java/me/exrates/ngcontroller/NgKYCControllerTest.java
@@ -0,0 +1,52 @@
+package me.exrates.ngcontroller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NgKYCControllerTest {
+
+    @Test
+    public void callback() {
+    }
+
+    @Test
+    public void callback1() {
+    }
+
+    @Test
+    public void getVerificationUrl() {
+    }
+
+    @Test
+    public void getVerificationStatus() {
+    }
+
+    @Test
+    public void getCountries() {
+    }
+
+    @Test
+    public void getLanguages() {
+    }
+
+    @Test
+    public void getCurrentVerificationStep() {
+    }
+
+    @Test
+    public void getStatusKyc() {
+    }
+
+    @Test
+    public void startKycProcessing() {
+    }
+
+    @Test
+    public void shuftiProExceptionHandler() {
+    }
+
+    @Test
+    public void exceptionHandler() {
+    }
+}
