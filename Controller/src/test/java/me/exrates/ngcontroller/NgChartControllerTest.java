9022e8f44811abd7642f5f136998f6581e32052f
Taranyk <one.37@yandex.ua>
2019-03-25 17:13:01 +0200

    NgChartControllerTest

---
+++ Controller/src/test/java/me/exrates/ngcontroller/NgChartControllerTest.java
@@ -0,0 +1,134 @@
+package me.exrates.ngcontroller;
+
+import me.exrates.model.CurrencyPair;
+import me.exrates.ngService.NgOrderService;
+import me.exrates.service.CurrencyService;
+import me.exrates.service.OrderService;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.anyList;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class NgChartControllerTest  extends AngularApiCommonTest  {
+
+    private final static String BASE_URL = "/api/public/v2/graph";
+
+    @Mock
+    private CurrencyService currencyService;
+    @Mock
+    private NgOrderService ngOrderService;
+    @Mock
+    private OrderService orderService;
+
+    @InjectMocks
+    private NgChartController ngChartController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(ngChartController)
+                .build();
+    }
+
+    @Test
+    public void getCandleChartHistoryData_WhenOk() {
+        CurrencyPair currencyPair = new CurrencyPair();
+
+
+        when(currencyService.getCurrencyPairByName(anyString())).thenReturn(currencyPair);
+    }
+
+    @Test
+    public void getCandleChartHistoryData_WhenNotFound() throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        when(currencyService.getCurrencyPairByName(anyString())).thenReturn(null);
+        when(ngOrderService.filterDataPeriod(anyList(), anyLong(),anyLong(),anyString())).thenReturn(map);
+
+        mockMvc.perform(get(BASE_URL + "/history")
+                .param("symbol", "freg")
+                .param("to", "2")
+                .param("from", "3")
+                .param("resolution", "deq")
+                .param("countback", "eqded"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getCandleTimeScaleMarks() {
+
+    }
+
+    @Test
+    public void getChartConfig() throws Exception {
+        mockMvc.perform(get(BASE_URL + "/config"))
+                .andExpect(jsonPath("$.*", hasSize(8)))
+                .andExpect(jsonPath("$.supports_search", is(true)))
+                .andExpect(jsonPath("$.supports_group_request", is(false)))
+                .andExpect(jsonPath("$.supports_marks", is(false)))
+                .andExpect(jsonPath("$.supports_timescale_marks", is(true)))
+                .andExpect(jsonPath("$.supports_time", is(true)))
+                .andExpect(jsonPath("$.exchanges", hasSize(2)))
+                .andExpect(jsonPath("$.exchanges[0].value", is("")))
+                .andExpect(jsonPath("$.exchanges[0].name", is("All Exchanges")))
+                .andExpect(jsonPath("$.exchanges[0].desc", is("")))
+                .andExpect(jsonPath("$.exchanges[1].value", is("EXRATES")))
+                .andExpect(jsonPath("$.exchanges[1].name", is("EXRATES")))
+                .andExpect(jsonPath("$.exchanges[1].desc", is("EXRATES")))
+                .andExpect(jsonPath("$.symbols_types", hasSize(1)))
+                .andExpect(jsonPath("$.symbols_types[0].name", is("All types")))
+                .andExpect(jsonPath("$.symbols_types[0].value", is("")))
+                .andExpect(jsonPath("$.supported_resolutions", hasSize(10)))
+                .andExpect(jsonPath("$.supported_resolutions.[0]", is("30")))
+                .andExpect(jsonPath("$.supported_resolutions.[9]", is("M")))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void getChartSymbol() throws Exception {
+        mockMvc.perform(get(BASE_URL + "/symbols")
+                .param("symbol", "symbols"))
+                .andExpect(jsonPath("$.*", hasSize(22)))
+                .andExpect(jsonPath("$.name", is("symbols")))
+                .andExpect(jsonPath("$.base_name", hasSize(1)))
+                .andExpect(jsonPath("$.base_name.[0]", is("symbols")))
+                .andExpect(jsonPath("$.full_name", is("symbols")))
+                .andExpect(jsonPath("$.minmov", is(1)))
+                .andExpect(jsonPath("$.fractional", is(false)))
+                .andExpect(jsonPath("$.type", is("bitcoin")))
+                .andExpect(jsonPath("$.ticker", is("symbols")))
+                .andExpect(jsonPath("$.supported_resolutions", hasSize(10)))
+                .andExpect(jsonPath("$.supported_resolutions.[0]", is("30")))
+                .andExpect(jsonPath("$.has_empty_bars", is(true)))
+                .andExpect(jsonPath("$.volume_precision", is(2)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getChartTime() throws Exception {
+        mockMvc.perform(get(BASE_URL + "/time"))
+                .andExpect(status().isOk());
+    }
+}
