b62d0def1864e38a28cd1afc28f41184edb8a797
Taranyk <one.37@yandex.ua>
2019-03-26 12:42:01 +0200

    up

--- Controller/src/test/java/me/exrates/ngcontroller/NgMailingControllerTest.java
+++ Controller/src/test/java/me/exrates/ngcontroller/NgMailingControllerTest.java
@@ -1,10 +1,33 @@
 package me.exrates.ngcontroller;
 
+import me.exrates.service.SendMailService;
+import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 public class NgMailingControllerTest {
+
+    private final static String BASE_URL = "/api/public/v2/listing";
+
+    @Mock
+    private SendMailService sendMailService;
+
+    @InjectMocks
+    private NgMailingController ngMailingController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(ngMailingController)
+                .build();
+    }
 
     @Test
     public void sendEmail() {
