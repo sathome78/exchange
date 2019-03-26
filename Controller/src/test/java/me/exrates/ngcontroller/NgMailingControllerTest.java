437f9969ad7758cfc62588b5cb06b9eccfaf83de
Taranyk <one.37@yandex.ua>
2019-03-26 11:53:46 +0200

    Start test

---
+++ Controller/src/test/java/me/exrates/ngcontroller/NgMailingControllerTest.java
@@ -0,0 +1,12 @@
+package me.exrates.ngcontroller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NgMailingControllerTest {
+
+    @Test
+    public void sendEmail() {
+    }
+}
