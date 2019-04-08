af1b26f48ba86badf3a11849f443f975bb0c51b6
olegpodolian <oleg.podolian@upholding.biz>
2019-04-08 12:03:06 +0300

    fix ieo invoice enum

--- Model/src/main/java/me/exrates/model/enums/invoice/IeoStatusEnum.java
+++ Model/src/main/java/me/exrates/model/enums/invoice/IeoStatusEnum.java
@@ -2,8 +2,6 @@ package me.exrates.model.enums.invoice;
 
 
 import lombok.extern.log4j.Log4j2;
-import me.exrates.model.Merchant;
-import me.exrates.model.enums.MerchantProcessType;
 import me.exrates.model.exceptions.InvoiceActionIsProhibitedForCurrencyPermissionOperationException;
 import me.exrates.model.exceptions.InvoiceActionIsProhibitedForNotHolderException;
 import me.exrates.model.exceptions.UnsupportedInvoiceStatusForActionException;
@@ -16,17 +14,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
-import static me.exrates.model.enums.invoice.InvoiceActionTypeEnum.ACCEPT_AUTO;
-import static me.exrates.model.enums.invoice.InvoiceActionTypeEnum.CREATE_BY_FACT;
-import static me.exrates.model.enums.invoice.InvoiceActionTypeEnum.CREATE_BY_USER;
 import static me.exrates.model.enums.invoice.InvoiceActionTypeEnum.InvoiceActionParamsValue;
-import static me.exrates.model.enums.invoice.InvoiceActionTypeEnum.PUT_FOR_CONFIRM_USER;
-import static me.exrates.model.enums.invoice.InvoiceActionTypeEnum.PUT_FOR_PENDING;
-import static me.exrates.model.enums.invoice.InvoiceActionTypeEnum.REQUEST_INNER_TRANSFER;
-import static me.exrates.model.enums.invoice.InvoiceActionTypeEnum.START_BCH_EXAMINE;
 
 /**
  * Created by ValkSam
@@ -84,31 +74,6 @@ public enum IeoStatusEnum implements InvoiceStatus {
     return availableForAction(schemaMap, action);
   }
 
-  static {
-    for (IeoStatusEnum status : IeoStatusEnum.class.getEnumConstants()) {
-      status.initSchema(status.schemaMap);
-    }
-    /*check schemaMap*/
-    getBeginState();
-  }
-
-  public static List<InvoiceStatus> getAvailableForActionStatusesList(InvoiceActionTypeEnum action) {
-    return Arrays.stream(IeoStatusEnum.class.getEnumConstants())
-        .filter(e -> e.availableForAction(action))
-        .collect(Collectors.toList());
-  }
-
-  public static List<InvoiceStatus> getAvailableForActionStatusesList(List<InvoiceActionTypeEnum> action) {
-    return Arrays.stream(IeoStatusEnum.class.getEnumConstants())
-        .filter(e -> action.stream().filter(e::availableForAction).findFirst().isPresent())
-        .collect(Collectors.toList());
-  }
-
-  public Set<InvoiceActionTypeEnum> getAvailableActionList() {
-    schemaMap.keySet().forEach(InvoiceActionTypeEnum::checkRestrictParamNeeded);
-    return schemaMap.keySet();
-  }
-
   public Set<InvoiceActionTypeEnum> getAvailableActionList(InvoiceActionParamsValue paramsValue) {
     return schemaMap.keySet()
             .stream()
@@ -148,35 +113,6 @@ public enum IeoStatusEnum implements InvoiceStatus {
     return candidateList.get(0);
   }
 
-  public static Set<InvoiceStatus> getMiddleStatesSet() {
-    return Arrays.stream(IeoStatusEnum.class.getEnumConstants())
-        .filter(e -> !e.schemaMap.isEmpty())
-        .collect(Collectors.toSet());
-  }
-
-  public static Set<InvoiceStatus> getEndStatesSet() {
-    return Arrays.stream(IeoStatusEnum.class.getEnumConstants())
-        .filter(e -> e.schemaMap.isEmpty())
-        .collect(Collectors.toSet());
-  }
-
-  public static InvoiceStatus getInvoiceStatusAfterAction(InvoiceActionTypeEnum action) {
-    TreeSet<InvoiceStatus> statusSet = new TreeSet(
-        Arrays.stream(IeoStatusEnum.class.getEnumConstants())
-            .filter(e -> e.availableForAction(action))
-            .map(e -> e.nextState(action))
-            .collect(Collectors.toList()));
-    if (statusSet.size() == 0) {
-      log.fatal("no state found !");
-      throw new AssertionError();
-    }
-    if (statusSet.size() > 1) {
-      log.fatal("more then one state found !");
-      throw new AssertionError();
-    }
-    return statusSet.first();
-  }
-
   @Override
   public Boolean isEndStatus() {
     return schemaMap.isEmpty();
@@ -189,9 +125,7 @@ public enum IeoStatusEnum implements InvoiceStatus {
         .forEach(e -> schema.putAll(e.schemaMap));
     return schema.entrySet().stream()
         .filter(e -> e.getValue() == this)
-        .filter(e -> e.getKey().isLeadsToSuccessFinalState())
-        .findAny()
-        .isPresent();
+        .anyMatch(e -> e.getKey().isLeadsToSuccessFinalState());
   }
 
   private static Set<InvoiceStatus> collectAllSchemaMapNodesSet() {
@@ -210,14 +144,6 @@ public enum IeoStatusEnum implements InvoiceStatus {
   @Override
   public Integer getCode() {
     return code;
-  }
-
-  public InvoiceActionTypeEnum getStartAction(Merchant merchant) {
-    if (merchant.getProcessType() == MerchantProcessType.INVOICE) {
-      return PUT_FOR_CONFIRM_USER;
-    } else {
-      return PUT_FOR_PENDING;
-    }
   }
 
 }
