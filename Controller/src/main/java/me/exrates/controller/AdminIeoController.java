6eb1554580b3b1efb6bd17ee7b1fe19377c74d5f
StasGorobzeev <stas.gorobzeev@tech-stack.io>
2019-04-20 11:54:53 +0300

    add save ieo details

--- Controller/src/main/java/me/exrates/controller/AdminIeoController.java
+++ Controller/src/main/java/me/exrates/controller/AdminIeoController.java
@@ -53,14 +53,14 @@ public class AdminIeoController {
                 .collect(Collectors.toList());
     }
 
-    @RequestMapping(value = "/2a8fy7b07dxe44/ieo", method = POST, consumes =  MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/2a8fy7b07dxe44/ieo", method = POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public ResponseEntity createIeo(@RequestBody @Valid IeoDetailsCreateDto dto) {
         ieoService.createIeo(dto);
         return new ResponseEntity(null, HttpStatus.CREATED);
     }
 
-    @RequestMapping(value = "/2a8fy7b07dxe44/ieo/{id}", method = PUT, consumes =  MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/2a8fy7b07dxe44/ieo/{id}", method = PUT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public ResponseEntity updateIeo(@PathVariable("id") Integer id, @RequestBody @Valid IeoDetailsUpdateDto dto) {
         ieoService.updateIeo(id, dto);
@@ -77,14 +77,13 @@ public class AdminIeoController {
     @RequestMapping(value = "/2a8fy7b07dxe44/ieo/approve/{id}", method = POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public ResponseEntity approveSuccessIeo(@PathVariable("id") Integer id) {
-        /*todo: call service method here*/
-        return ResponseEntity.ok(null);
+        ieoService.approveSuccessIeo(id, getPrincipalEmail());
+        return ResponseEntity.ok().build();
     }
 
     private String getPrincipalEmail() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
-
 
 
 }
