50ba520e8d57c17200c658635d995e13bde3a15f
NMaslukov <JavaNoob14679987>
2019-05-14 20:56:03 +0300

    SWITCH_INOUT_BY_REDIS done

--- Model/src/main/java/me/exrates/model/condition/MonolitConditional.java
+++ Model/src/main/java/me/exrates/model/condition/MonolitConditional.java
@@ -3,7 +3,9 @@ package me.exrates.model.condition;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MonolitConditional implements Condition {
 
     private final MicroserviceConditional microserviceConditional = new MicroserviceConditional();
