1f820cd8315b589ffd675c5557698250cf23c3b3
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-07-01 17:29:35 +0300

    DEVEX-2530-prod (save country code when processing KYC)

--- Dao/src/main/java/me/exrates/dao/UserDao.java
+++ Dao/src/main/java/me/exrates/dao/UserDao.java
@@ -7,13 +7,25 @@ import me.exrates.model.Policy;
 import me.exrates.model.TemporalToken;
 import me.exrates.model.User;
 import me.exrates.model.UserFile;
-import me.exrates.model.dto.*;
+import me.exrates.model.dto.IpLogDto;
+import me.exrates.model.dto.UpdateUserDto;
+import me.exrates.model.dto.UserBalancesDto;
+import me.exrates.model.dto.UserCurrencyOperationPermissionDto;
+import me.exrates.model.dto.UserIpDto;
+import me.exrates.model.dto.UserIpReportDto;
+import me.exrates.model.dto.UserSessionInfoDto;
+import me.exrates.model.dto.UserShortDto;
+import me.exrates.model.dto.UsersInfoDto;
 import me.exrates.model.dto.dataTable.DataTableParams;
 import me.exrates.model.dto.filterData.AdminIpLogsFilterData;
 import me.exrates.model.dto.ieo.IeoUserStatus;
 import me.exrates.model.dto.kyc.EventStatus;
 import me.exrates.model.dto.mobileApiDto.TemporaryPasswordDto;
-import me.exrates.model.enums.*;
+import me.exrates.model.enums.NotificationMessageEventEnum;
+import me.exrates.model.enums.PolicyEnum;
+import me.exrates.model.enums.TokenType;
+import me.exrates.model.enums.UserEventEnum;
+import me.exrates.model.enums.UserRole;
 import me.exrates.model.enums.invoice.InvoiceOperationDirection;
 import me.exrates.model.enums.invoice.InvoiceOperationPermission;
 
@@ -196,7 +208,7 @@ public interface UserDao {
 
     boolean manageUserFavouriteCurrencyPair(int userId, int currencyPairId, boolean delete);
 
-    int updateReferenceIdAndStatus(String referenceId, EventStatus status, String userEmail);
+    boolean updateReferenceIdAndStatus(String referenceId, EventStatus status, String userEmail);
 
     String getReferenceIdByUserEmail(String userEmail);
 
@@ -242,4 +254,8 @@ public interface UserDao {
     IeoUserStatus findIeoUserStatusByEmail(String email);
 
     boolean updateUserRole(int userId, UserRole userRole);
+
+    String getUserPublicId(String userEmail);
+
+    boolean updateCountryCode(String countryCode, String userEmail);
 }
