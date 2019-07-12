1cdb542caa0294a45596113a4c9a8e380abd6325
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-07-12 15:09:26 +0300

    DEVEX-2205 (fix conflicts / delete migration files)

--- Controller/src/main/resources/db/migration/V1.201906211402__EOS_ScannedBlock_Up.sql
+++
@@ -1,3 +0,0 @@
-UPDATE MERCHANT_SPEC_PARAMETERS
-set param_value = 64653000
-where merchant_id = (SELECT id FROM MERCHANT WHERE name = 'EOS');
--- Controller/src/main/resources/db/migration/V1.201907081653__update_qubera_settings.sql
+++
@@ -1,7 +0,0 @@
-UPDATE MERCHANT_CURRENCY
-SET withdraw_auto_enabled = 1, withdraw_auto_threshold_amount = 1
-WHERE merchant_id = (
-  SELECT id
-  FROM MERCHANT
-  WHERE name = 'Qubera'
-);
