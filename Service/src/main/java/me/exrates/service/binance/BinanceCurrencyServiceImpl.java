a888998dc4f89bc45f25fa89c413dc02d5013607
gregory.taranik <gregory.taranik@upholding.biz>
2019-07-30 16:52:58 +0300

    temp

--- Service/src/main/java/me/exrates/service/binance/BinanceCurrencyServiceImpl.java
+++ Service/src/main/java/me/exrates/service/binance/BinanceCurrencyServiceImpl.java
@@ -7,13 +7,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 import me.exrates.model.condition.MonolitConditional;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
@@ -28,21 +24,24 @@ public class BinanceCurrencyServiceImpl implements BinanceCurrencyService {
 
     @Autowired
     public BinanceCurrencyServiceImpl(){
-//        CloseableHttpClient httpClient = HttpClients.custom()
-//                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-//                .build();
-//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-//        requestFactory.setHttpClient(httpClient);
         restTemplate = new RestTemplate();
     }
 
-    public int getAddressInfo(int num){
+    public static void main(String[] args) {
+        BinanceCurrencyServiceImpl binanceCurrencyService = new BinanceCurrencyServiceImpl();
+
+//                System.out.println(binanceCurrencyService.getBlockInfo(2357613));
+                System.out.println(binanceCurrencyService.getTransactions(""));
+                //        System.out.println(binanceCurrencyService.checkTransaction("CA4394B114376FF06AEA55866DFF5CD058F591AD3A18E28B34DF502E66AE796B"));
+    }
+
+    public String getTransactions(String hash){
         UriComponents builder = UriComponentsBuilder
-                .fromHttpUrl("http://172.31.30.170:27147/block?height={num}")
+                .fromHttpUrl("http://172.31.30.170:27147/tx_search?query=\"tx.height=2357614\"&prove=true")
                 .build();
-        ResponseEntity<Response> responseEntity = null;
+        ResponseEntity<String> responseEntity = null;
         try {
-            responseEntity = restTemplate.getForEntity(builder.toUriString(), Response.class, num);
+            responseEntity = restTemplate.getForEntity(builder.toUriString(), String.class, hash);
             if (responseEntity.getStatusCodeValue() != 200) {
                 log.error("Error : {}", responseEntity.getStatusCodeValue());
             }
@@ -50,7 +49,24 @@ public class BinanceCurrencyServiceImpl implements BinanceCurrencyService {
             log.error("Error : {}", ex.getMessage());
         }
 
-        return responseEntity.getBody().result.block_meta.header.num_txs;
+        return responseEntity.getBody();
+    }
+
+    public String getBlockInfo(int num){
+        UriComponents builder = UriComponentsBuilder
+                .fromHttpUrl("http://172.31.30.170:27147/block?height={num}")
+                .build();
+        ResponseEntity<String> responseEntity = null;
+        try {
+            responseEntity = restTemplate.getForEntity(builder.toUriString(), String.class, num);
+            if (responseEntity.getStatusCodeValue() != 200) {
+                log.error("Error : {}", responseEntity.getStatusCodeValue());
+            }
+        } catch (Exception ex) {
+            log.error("Error : {}", ex.getMessage());
+        }
+
+        return responseEntity.getBody();
     }
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -107,14 +123,5 @@ public class BinanceCurrencyServiceImpl implements BinanceCurrencyService {
         return responseEntity.getBody();
     }
 
-    public static void main(String[] args) {
-        BinanceCurrencyServiceImpl binanceCurrencyService = new BinanceCurrencyServiceImpl();
-        int numb = 580500;
-        while (++numb <= 800000){
-            if (binanceCurrencyService.getAddressInfo(numb) > 0){
-                System.out.println(numb + " : " + binanceCurrencyService.getAddressInfo(numb));
-            }
-        }
-//        System.out.println(binanceCurrencyService.checkTransaction("CA4394B114376FF06AEA55866DFF5CD058F591AD3A18E28B34DF502E66AE796B"));
-    }
+
 }
