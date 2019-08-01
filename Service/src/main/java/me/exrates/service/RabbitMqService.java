454d138f39db777daeadbbe72ffefea0ed0a56c8
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-08-01 14:13:21 +0300

    DEVEX-3262 (return back some code)

--- Service/src/main/java/me/exrates/service/RabbitMqService.java
+++ Service/src/main/java/me/exrates/service/RabbitMqService.java
@@ -5,5 +5,9 @@ import me.exrates.model.ExOrder;
 
 public interface RabbitMqService {
 
+    String REFILL_QUEUE = "refill";
+
+//    void sendOrderInfo(InputCreateOrderDto inputOrder, String queueName);
+
     void sendTradeInfo(ExOrder order);
 }
--- Service/src/main/java/me/exrates/service/impl/RabbitMqServiceImpl.java
+++ Service/src/main/java/me/exrates/service/impl/RabbitMqServiceImpl.java
@@ -1,5 +1,6 @@
 package me.exrates.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
 import me.exrates.model.CurrencyPair;
 import me.exrates.model.ExOrder;
@@ -9,6 +10,7 @@ import me.exrates.service.chart.TradeDataDto;
 import me.exrates.service.exception.RabbitMqException;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -18,16 +20,40 @@ public class RabbitMqServiceImpl implements RabbitMqService {
 
     private final CurrencyService currencyService;
     private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper mapper;
 
     private String chartQueue;
 
+    @Autowired
     public RabbitMqServiceImpl(CurrencyService currencyService,
                                RabbitTemplate rabbitTemplate,
+                               ObjectMapper mapper,
                                @Value("${rabbit.chart.queue}") String chartQueue) {
         this.currencyService = currencyService;
         this.rabbitTemplate = rabbitTemplate;
+        this.mapper = mapper;
         this.chartQueue = chartQueue;
     }
+
+//    @Override
+//    public void sendOrderInfo(InputCreateOrderDto inputOrder, String queueName) {
+//        try {
+//            String orderJson = mapper.writeValueAsString(inputOrder);
+//            log.info("Sending order to demo-server {}", orderJson);
+//
+//            try {
+//                byte[] bytes = (byte[]) this.rabbitTemplate.convertSendAndReceive(queueName, orderJson);
+//                String result = new String(bytes);
+//                log.info("Return from demo-server {}", result);
+//            } catch (AmqpException e) {
+//                String msg = "Failed to send data via rabbit queue";
+//                log.error(msg + " " + orderJson, e);
+//                throw new RabbitMqException(msg);
+//            }
+//        } catch (JsonProcessingException e) {
+//            log.error("Failed to send order to old instance", e);
+//        }
+//    }
 
     @Override
     public void sendTradeInfo(ExOrder order) {
