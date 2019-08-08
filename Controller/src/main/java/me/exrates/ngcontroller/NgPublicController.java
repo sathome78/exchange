de1f2119ab2e003cb83cba6dbfcdf05a93a24af2
Maxim Peresadko <maxim.peresadko@upholding.biz>
2019-08-08 15:06:16 +0300

    logs remove

--- Controller/src/main/java/me/exrates/ngcontroller/NgPublicController.java
+++ Controller/src/main/java/me/exrates/ngcontroller/NgPublicController.java
@@ -36,8 +36,6 @@ import me.exrates.service.exception.IllegalChatMessageException;
 import me.exrates.service.notifications.G2faService;
 import me.exrates.service.util.IpUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
