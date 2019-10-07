c6507867e37123abcd27cbfefe2841d740abc151
gregory.taranik <gregory.taranik@upholding.biz>
2019-10-07 16:31:45 +0300

    Up EOS coin

--- Service/src/main/java/me/exrates/service/eos/EosReceiveService.java
+++ Service/src/main/java/me/exrates/service/eos/EosReceiveService.java
@@ -97,7 +97,7 @@ public class EosReceiveService {
                     });
                 }
             });
-            if (lastBlock % 500 == 0) {
+            if (lastBlock % 500 == 0){
                 saveLastBlock(lastBlock);
             }
         }
