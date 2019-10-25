b216adff83bc11c777bcd2d8bfbc42e67358867e
maks.hruslov <maks.hruslov@upholding.biz>
2019-10-25 14:04:45 +0300

    Change sql/remove fails from db

--- Controller/src/main/resources/db/migration/V1.201909161820__TRON_USDT_TRC20.sql
+++ Controller/src/main/resources/db/migration/V1.201909161820__TRON_USDT_TRC20.sql
@@ -6,7 +6,6 @@ VALUES ('USDT(TRX)', 'Tether USD TRC-20', 0, 8, 8, 8);
 
 INSERT IGNORE INTO COMPANY_WALLET_EXTERNAL(currency_id) VALUES ((SELECT id from CURRENCY WHERE name='USDT(TRX)'));
 
-
 INSERT IGNORE INTO MERCHANT_CURRENCY (merchant_id, currency_id, min_sum)
   VALUES ((SELECT id from MERCHANT WHERE name='USDT(TRX)'),
           (SELECT id from CURRENCY WHERE name='USDT(TRX)'),
@@ -23,24 +22,9 @@ INSERT IGNORE INTO CURRENCY_LIMIT(currency_id, operation_type_id, user_role_id, 
 
 INSERT IGNORE INTO `COMPANY_WALLET` (`currency_id`) VALUES ((select id from CURRENCY where name = 'USDT(TRX)'));
 
-INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, ticker_name)
-VALUES((select id from CURRENCY where name = 'USDT(TRX)'), (select id from CURRENCY where name = 'TRX'), 'USDT(TRX)/TRX', 170, 0, 'USDT(TRX)/TRX');
-
-INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
-  SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
-  JOIN USER_ROLE UR
-  JOIN ORDER_TYPE OT where CP.name='TRX/USDT(TRX)';
-
-INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, ticker_name)
-VALUES((select id from CURRENCY where name = 'USDT(TRX)'), (select id from CURRENCY where name = 'BTT'), 'USDT(TRX)/BTT', 170, 0, 'USDT(TRX)/BTT');
-
-INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
-  SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
-  JOIN USER_ROLE UR
-  JOIN ORDER_TYPE OT where CP.name='BTT/USDT(TRX)';
 
 INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,ticker_name)
-VALUES((select id from CURRENCY where name = 'USDT(TRX)'), (select id from CURRENCY where name = 'BTC'), 'BTC/USDT(TRX)', 160, 0, 'BTC', 'BTC/USDT(TRX)');
+VALUES((select id from CURRENCY where name = 'USDT(TRX)'), (select id from CURRENCY where name = 'BTC'), 'BTC/USDT(TRX)', 160, 0, 'USDT', 'BTC/USDT(TRX)');
 
 INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
   SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
@@ -48,12 +32,28 @@ INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_ty
     JOIN ORDER_TYPE OT where CP.name='BTC/USDT(TRX)';
 
 INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,ticker_name)
-VALUES((select id from CURRENCY where name = 'USDT(TRX)'), (select id from CURRENCY where name = 'ETH'), 'ETH/USDT(TRX)', 160, 0, 'ETH', 'ETH/USDT(TRX)');
+VALUES((select id from CURRENCY where name = 'USDT(TRX)'), (select id from CURRENCY where name = 'ETH'), 'ETH/USDT(TRX)', 160, 0, 'USDT', 'ETH/USDT(TRX)');
 
 INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
   SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
     JOIN USER_ROLE UR
     JOIN ORDER_TYPE OT where CP.name='ETH/USDT(TRX)';
+
+INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market, ticker_name)
+VALUES((select id from CURRENCY where name = 'USDT(TRX)'), (select id from CURRENCY where name = 'TRX'), 'TRX/USDT(TRX)', 170, 0, 'USDT', 'TRX/USDT(TRX)');
+
+INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
+SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
+    JOIN USER_ROLE UR
+    JOIN ORDER_TYPE OT where CP.name='TRX/USDT(TRX)';
+
+INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market, ticker_name)
+VALUES((select id from CURRENCY where name = 'USDT(TRX)'), (select id from CURRENCY where name = 'BTT'), 'BTT/USDT(TRX)', 170, 0, 'USDT', 'BTT/USDT(TRX)');
+
+INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
+SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
+    JOIN USER_ROLE UR
+    JOIN ORDER_TYPE OT where CP.name='BTT/USDT(TRX)';
 
 INSERT IGNORE INTO MERCHANT_CURRENCY (merchant_id, currency_id, min_sum, withdraw_block, refill_block, transfer_block)
 VALUES ((SELECT id FROM MERCHANT WHERE name = 'SimpleTransfer'), (select id from CURRENCY where name = 'USDT(TRX)'), 0.0001, 1, 1, 0);
